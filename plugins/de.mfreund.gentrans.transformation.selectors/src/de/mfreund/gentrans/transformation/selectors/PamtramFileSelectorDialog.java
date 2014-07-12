fca68e41c6345da0e6358d9476eb4fdfa1650ea7
Sascha Steffen <steffensascha@gmail.com>
2014-07-12 22:43:11 +0200

    selecting target file

--- plugins/de.mfreund.gentrans.transformation.selectors/src/de/mfreund/gentrans/transformation/selectors/PamtramFileSelectorDialog.java
+++ plugins/de.mfreund.gentrans.transformation.selectors/src/de/mfreund/gentrans/transformation/selectors/PamtramFileSelectorDialog.java
@@ -6,21 +6,38 @@ import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class PamtramFileSelectorDialog extends Object {
-	private static String file="";
+	private static String pamtramFile="";
+	private static String targetFile="";
+	public static String getPamtramFile() {
+		return pamtramFile;
+	}
+	public static String getTargetFile() {
+		return targetFile;
+	}
 	public static String run(final String startPath){
 		Display.getDefault().syncExec(new Runnable(){
 			public void run(){
 				Display display= Display.getDefault();
 				Shell shell=new Shell(display);
-				FileDialog d=new FileDialog(shell, SWT.APPLICATION_MODAL);
+				FileDialog d=new FileDialog(shell, SWT.APPLICATION_MODAL | SWT.OPEN);
 				d.setText("Select a PAMTraM Model to transform this XML model");
 				d.setFilterPath(startPath);
 				d.setFilterExtensions(new String[]{"*.pamtram"});
-				file=d.open();
+				pamtramFile=d.open();
+				
+				d=new FileDialog(shell, SWT.APPLICATION_MODAL | SWT.SAVE);
+				d.setText("Where should the target Model be saved?");
+				d.setFilterPath(startPath);
+				d.setFilterExtensions(new String[]{"*.xmi"});				
+				d.setOverwrite(true);
+				
+				targetFile = d.open();
+				System.out.println(targetFile);
+				
 				
 			};
 		});
-		return file;
+		return pamtramFile;
 	};	
 
 

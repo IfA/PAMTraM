839a6d1d4065f6addf90a38b448094b16f35d01e
mfreund <matth.freund@gmail.com>
2014-07-22 14:18:07 +0200

    * implement the 'addSourcePropertyDescriptor

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/SimpleAttributeMappingItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/SimpleAttributeMappingItemProvider.java
@@ -3,14 +3,27 @@
 package pamtram.mapping.provider;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+
+import pamtram.mapping.AttributeMappingSourceElementType;
+import pamtram.mapping.Mapping;
+import pamtram.mapping.MappingHintGroup;
 import pamtram.mapping.MappingPackage;
 import pamtram.mapping.SimpleAttributeMapping;
+import pamtram.metamodel.SourceSectionClass;
+import pamtram.metamodel.SourceSectionNonContainmentReference;
 
 /**
  * This is the item provider adapter for a {@link pamtram.mapping.SimpleAttributeMapping} object.
@@ -54,19 +67,70 @@ public class SimpleAttributeMappingItemProvider
 	 * @generated
 	 */
 	protected void addSourcePropertyDescriptor(Object object) {
+		// copied from 'AttributeMappingSourceElementTypeItemProvider'
+		// (not automatically performed due to multiple inheritance
 		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_AttributeMappingSourceElementType_source_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_AttributeMappingSourceElementType_source_feature", "_UI_AttributeMappingSourceElementType_type"),
-				 MappingPackage.Literals.ATTRIBUTE_MAPPING_SOURCE_ELEMENT_TYPE__SOURCE,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+		(new ItemPropertyDescriptor
+			(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+			 getResourceLocator(),
+			 getString("_UI_AttributeMappingSourceElementType_source_feature"),
+			 getString("_UI_PropertyDescriptor_description", "_UI_AttributeMappingSourceElementType_source_feature", "_UI_AttributeMappingSourceElementType_type"),
+			 MappingPackage.Literals.ATTRIBUTE_MAPPING_SOURCE_ELEMENT_TYPE__SOURCE,
+			 true,
+			 false,
+			 true,
+			 null,
+			 null,
+			 null)
+		   {
+			@Override
+			public Collection<?> getChoiceOfValues(Object object) {
+
+				//the parent Mapping Hint Group
+				EObject parent=((AttributeMappingSourceElementType) object).eContainer();
+				MappingHintGroup mappingHintGroup;
+				while(true){
+					if(parent instanceof MappingHintGroup){
+						mappingHintGroup=(MappingHintGroup) parent;
+						break;
+					} else {
+						parent=parent.eContainer();
+					}
+				}
+				// the parent mapping
+				Mapping mapping = (Mapping) mappingHintGroup.eContainer();
+				
+				// the source section
+				SourceSectionClass source = mapping.getSourceMMSection();
+
+				List<Object> choiceOfValues = new ArrayList<Object>();
+				
+				// iterate over all elements and return the attributes as possible options
+				Set<SourceSectionClass> scanned=new HashSet<SourceSectionClass>();
+				List<SourceSectionClass> sectionsToScan=new ArrayList<SourceSectionClass>();
+				sectionsToScan.add(source);
+				
+				while(sectionsToScan.size() > 0){
+					SourceSectionClass classToScan=sectionsToScan.remove(0);
+					scanned.add(classToScan);
+					
+					Iterator<EObject> it = classToScan.eAllContents();
+					while(it.hasNext()) {
+						EObject next = it.next();
+						if(next instanceof pamtram.metamodel.Attribute) {
+							choiceOfValues.add(next);
+						} else if(next instanceof SourceSectionNonContainmentReference){
+							List<SourceSectionClass> vals=new ArrayList<SourceSectionClass>();
+							vals.addAll(((SourceSectionNonContainmentReference) next).getValue());
+							vals.removeAll(scanned);
+							sectionsToScan.addAll(vals);
+						}
+					}
+				}
+				
+				return choiceOfValues;
+			}
+		   });
 	}
 
 	/**

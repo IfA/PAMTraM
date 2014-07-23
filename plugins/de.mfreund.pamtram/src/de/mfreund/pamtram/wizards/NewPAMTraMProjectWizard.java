4da40acc575f437bcf313278d906ee82db0d9bf0
mfreund <matth.freund@gmail.com>
2014-07-23 17:03:33 +0200

    * initial import * basic new project wizard that creates a project and a simple pamtram model

---
+++ plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/wizards/NewPAMTraMProjectWizard.java
@@ -0,0 +1,252 @@
+package de.mfreund.pamtram.wizards;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.ide.undo.CreateProjectOperation;
+import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
+
+import pamtram.presentation.PamtramEditorPlugin;
+import pamtram.presentation.PamtramModelWizard;
+import pamtram.presentation.pages.PamtramEPackageSpecificationPage;
+
+public class NewPAMTraMProjectWizard extends PamtramModelWizard {
+
+	// the element currently selected (might e.g. be a working set)
+	private IStructuredSelection selection;
+	// the project to be created
+	private IProject newProject;
+	private WizardNewProjectCreationPage mainPage;
+
+	/**
+	 * Constructor for the NewPAMTraMProjectWizard
+	 */
+	public NewPAMTraMProjectWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		super.init(workbench, selection);
+		setWindowTitle("PAMTraM Project Wizard");
+	}
+	
+	@Override
+	public void addPages() {
+		
+		mainPage = new WizardNewProjectCreationPage("autoProBeProjectCreation") { //$NON-NLS-1$
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see org.eclipse.ui.dialogs.WizardNewProjectCreationPage#createControl(org.eclipse.swt.widgets.Composite)
+			 */
+			public void createControl(Composite parent) {
+				super.createControl(parent);
+				createWorkingSetGroup(
+						(Composite) getControl(),
+						selection,
+						new String[] {"org.eclipse.ui.resourceWorkingSetPage"}); //$NON-NLS-1$
+				Dialog.applyDialogFont(getControl());
+			}
+		}; 
+		mainPage.setTitle("PAMTraM Project");
+		mainPage.setDescription("This creates a new PAMTraM Project that consists of a PAMTraM model.");
+		this.addPage(mainPage);
+
+		ePackageSpecificationPage = new PamtramEPackageSpecificationPage("Whatever3");
+		ePackageSpecificationPage.setTitle("ePackage specification");
+		ePackageSpecificationPage.setDescription("Specify the ePackages of the source and target sections.");
+		this.addPage(ePackageSpecificationPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+		/*
+		 * Create the Project 
+		 */
+		try {
+			createNewProject();
+		} catch (InvocationTargetException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (newProject == null) {
+			return false;
+		}
+		
+//		IWorkingSet[] workingSets = mainPage.getSelectedWorkingSets();
+//		workbench.getWorkingSetManager().addToWorkingSets(newProject.getProject(),
+//				workingSets);
+		
+		
+		doFinish();
+		
+		return true;
+	}
+	
+	/**
+	 * Creates a new project resource with the selected name.
+	 * <p>
+	 * In normal usage, this method is invoked after the user has pressed Finish
+	 * on the wizard; the enablement of the Finish button implies that all
+	 * controls on the pages currently contain valid values.
+	 * </p>
+	 * <p>
+	 * Note that this wizard caches the new project once it has been
+	 * successfully created; subsequent invocations of this method will answer
+	 * the same project resource without attempting to create it again.
+	 * </p>
+	 * 
+	 * @return the created project resource, or <code>null</code> if the
+	 *         project was not created
+	 * @throws InvocationTargetException 
+	 * @throws CoreException 
+	 */
+	private IProject createNewProject() throws InvocationTargetException, CoreException {
+		if (newProject != null) {
+			return newProject;
+		}
+
+		// get a project handle
+		IProject newProjectHandle = mainPage.getProjectHandle();
+		
+
+		// get a project descriptor
+		URI location = null;
+		if (!mainPage.useDefaults()) {
+			location = mainPage.getLocationURI();
+		}
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		final IProjectDescription description = workspace
+				.newProjectDescription(newProjectHandle.getName());
+		description.setLocationURI(location);
+//		description.setNatureIds(new String[]{"org.eclipse.wst.common.project.facet.core.nature", "org.eclipse.jdt.core.javanature", "org.eclipse.wst.common.modulecore.ModuleCoreNature", "org.eclipse.jem.workbench.JavaEMFNature"});
+//		description.setBuildConfigs(new String[]{"org.eclipse.wst.common.project.facet.core.builder"});
+		
+		// create the new project operation
+		IRunnableWithProgress op = new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor)
+					throws InvocationTargetException {
+				CreateProjectOperation op = new CreateProjectOperation(
+						description, "PAMTraM Project");
+				try {
+					// see bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=219901
+					// directly execute the operation so that the undo state is
+					// not preserved.  Making this undoable resulted in too many 
+					// accidental file deletions.
+					op.execute(monitor, WorkspaceUndoUtil
+						.getUIInfoAdapter(getShell()));
+				} catch (ExecutionException e) {
+					throw new InvocationTargetException(e);
+				}
+			}
+		};
+
+		// run the new project creation operation
+		try {
+			getContainer().run(true, true, op);
+		} catch (InterruptedException e) {
+			return null;
+		} catch (InvocationTargetException e) {
+			throw new InvocationTargetException(e);
+		}
+		
+//		IJavaProject javaProject = JavaCore.create(newProjectHandle);
+//		// set the source folder
+//		IClasspathEntry srcEntry = JavaCore.newSourceEntry(newProjectHandle.getFolder("src").getFullPath());
+//		javaProject.setRawClasspath(new IClasspathEntry[]{srcEntry}, null);
+//		// set defa setOutputLocation(newProjectHandle.getFolder("build/classes").getFullPath(), null);
+//		
+		// create a faceted project and add the dynamic web and java facets
+//		newProject = ProjectManager.create(newProjectHandle);
+		newProject = newProjectHandle.getProject();
+//		addFacets(newProject);
+		
+		return newProject;
+	}
+	
+	/**
+	 * The worker mehthod.
+	 */
+	private void doFinish() {
+		/*
+		 * Copy and load the STGML model and generate the Movisa model
+		 */
+		WorkspaceModifyOperation operation =
+			new WorkspaceModifyOperation() {
+				@Override
+				protected void execute(IProgressMonitor progressMonitor) {
+					try {
+						// Create a resource set
+						//
+						ResourceSet resourceSet = new ResourceSetImpl();
+
+						// Get the URI of the model file.
+						//
+						org.eclipse.emf.common.util.URI fileURI = 
+								org.eclipse.emf.common.util.URI.createPlatformResourceURI(newProject.getFile("my.pamtram").getFullPath().toString(), true);
+
+						// Create a resource for this file.
+						//
+						Resource resource = resourceSet.createResource(fileURI);
+
+						// Add the initial model object to the contents.
+						//
+						EObject rootObject = createInitialModel();
+						if (rootObject != null) {
+							resource.getContents().add(rootObject);
+						}
+
+						// Save the contents of the resource to the file system.
+						//
+						Map<Object, Object> options = new HashMap<Object, Object>();
+//						options.put(XMLResource.OPTION_ENCODING, initialObjectCreationPage.getEncoding());
+						resource.save(null);
+					}
+					catch (Exception exception) {
+						exception.printStackTrace();
+					}
+					finally {
+						progressMonitor.done();
+					}
+				}
+		};
+
+		try {
+			getContainer().run(true, false, operation);
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}

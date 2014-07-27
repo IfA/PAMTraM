944ce38e0a386a5bc61066a80465c536158ca0a0
mfreund <matth.freund@gmail.com>
2014-07-27 13:50:01 +0200

    * implement the 'setDefaults' method of the main tab

--- plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/launching/GentransLaunchMainTab.java
+++ plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/launching/GentransLaunchMainTab.java
@@ -3,7 +3,10 @@ package de.mfreund.pamtram.launching;
 import java.io.File;
 import java.util.Arrays;
 
+import org.eclipse.core.internal.resources.Project;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -18,6 +21,7 @@ import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
@@ -113,11 +117,13 @@ public class GentransLaunchMainTab extends AbstractLaunchConfigurationTab {
 				pamtramFileCombo.select(0);
 				pamtramFileCombo.setEnabled(true);
 				// update the target file combo
+				targetFileCombo.add("out.xmi");
 				for(File f : new File(projectPath + Path.SEPARATOR + "Target").listFiles()) {
 					if(f.getName().endsWith(".xmi")) {
 						targetFileCombo.add(f.getName());
 					}
 				}
+				targetFileCombo.select(0);
 				targetFileCombo.setEnabled(true);
 				updateLaunchConfigurationDialog();
 			}
@@ -200,11 +206,90 @@ public class GentransLaunchMainTab extends AbstractLaunchConfigurationTab {
 
 	@Override
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
-//		// get the active shell
-//		IWorkbench wb = PlatformUI.getWorkbench(); 
-//		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-//		ISelection selection = win.getSelectionService().getSelection();
-//		if(selection.)
+		
+		// get the current selection
+		IWorkbench wb = PlatformUI.getWorkbench(); 
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		ISelection sel = win.getSelectionService().getSelection(); 
+		
+		// nothing can be done if the user did not select anything
+		if(sel.isEmpty()) {
+			return;
+		}
+		
+		// handle a selection in a tree viewer
+		if(sel instanceof TreeSelection) {
+			TreeSelection treeSel = (TreeSelection) win.getSelectionService().getSelection();
+			Object el = treeSel.getFirstElement();
+			
+			IProject project;
+			IFile srcFile = null, pamtramFile = null;
+			
+			// determine the project based on the selection
+			if(el instanceof IProject) {
+				project = (IProject) el; 
+			} else if(el instanceof IFile) {
+				project = ((IFile) el).getProject();
+				IFile file = (IFile) el;
+				// check if the file can be used as source file...
+				if(file.getName().endsWith(".xmi") && 
+						file.getParent().getName().equals("Source")) {
+					srcFile = file;
+				// ... or as pamtram file
+				} else if(file.getName().endsWith(".pamtram") &&
+						file.getParent().getName().equals("Pamtram")) {
+					pamtramFile = file;
+				}
+			} else {
+				return;
+			}
+			
+			try {
+				
+				// check if the project has the pamtram nature assigned
+				if(project.hasNature("de.mfreund.pamtram.pamtramNature")) {
+					// set the project attribute
+					configuration.setAttribute("project", 
+							project.getLocation().toOSString());
+					
+					// set the srcFile attribute
+					if(srcFile == null) {
+						for(IResource res : project.getFolder("Source").members()) {
+							// search for a suitable src file
+							if(res instanceof IFile && ((IFile) res).getName().endsWith(".xmi")) {
+								srcFile = (IFile) res;
+								configuration.setAttribute("srcFile", srcFile.getName());
+								break;
+							}
+						}
+					} else {
+						configuration.setAttribute("srcFile", srcFile.getName());
+					}
+					// set the pamtramFile attribute
+					if(pamtramFile == null) {
+						for(IResource res : project.getFolder("Pamtram").members()) {
+							// search for a suitable pamtram file
+							if(res instanceof IFile && ((IFile) res).getName().endsWith(".pamtram")) {
+								pamtramFile = (IFile) res;
+								configuration.setAttribute("pamtramFile", pamtramFile.getName());
+								break;
+							}
+						}
+					} else {
+						configuration.setAttribute("pamtramFile", pamtramFile.getName());
+					}
+					// set the targetFile attribute
+					configuration.setAttribute("targetFile", "out.xmi");
+					
+				} else {
+					return;
+				}
+				
+			} catch (CoreException e) {
+				setErrorMessage(e.getMessage());
+				return;
+			}
+		}
 	}
 
 	@Override

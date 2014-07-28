de7b340654af1c294e6d47787913e48464327415
Sascha Steffen <steffensascha@gmail.com>
2014-07-28 10:42:00 +0200

    narrowed down choices for ClassMatcher

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/ClassMatcherItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/ClassMatcherItemProvider.java
@@ -4,12 +4,21 @@ package pamtram.mapping.provider;
 
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+
+import pamtram.mapping.MappingInstanceSelector;
 import pamtram.mapping.MappingPackage;
+import pamtram.metamodel.TargetSectionClass;
 
 /**
  * This is the item provider adapter for a {@link pamtram.mapping.ClassMatcher} object.
@@ -53,7 +62,7 @@ public class ClassMatcherItemProvider
 	 */
 	protected void addTargetClassPropertyDescriptor(Object object) {
 		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
+			(new ItemPropertyDescriptor
 				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
 				 getResourceLocator(),
 				 getString("_UI_ClassMatcher_targetClass_feature"),
@@ -64,7 +73,37 @@ public class ClassMatcherItemProvider
 				 true,
 				 null,
 				 null,
-				 null));
+				 null){
+
+					@Override
+					public Collection<?> getChoiceOfValues(Object object) {
+						Collection<?> superChoices=super.getChoiceOfValues(object);
+						
+						EClass refClass=null;
+						try{
+							refClass=((MappingInstanceSelector) ((EObject) object).eContainer()).getAffectedReference().getEReference().getEReferenceType();
+						} catch(Exception e){
+							return superChoices;
+						}
+							List<TargetSectionClass> choices=new LinkedList<TargetSectionClass>();
+							Iterator<?>  it=superChoices.iterator();
+							while(it.hasNext()){
+								Object next=it.next();
+								if(next instanceof TargetSectionClass){
+									if(((TargetSectionClass) next).getEClass() != null){
+										if(refClass.isSuperTypeOf(((TargetSectionClass) next).getEClass())){
+											choices.add((TargetSectionClass) next);
+										}
+									}
+								}
+							}
+							
+							return choices;
+						
+					}
+				
+				
+			});
 	}
 
 	/**

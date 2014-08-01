aa423b5332717de25f1994e622f2fb5b2b53a509
mfreund <matth.freund@gmail.com>
2014-08-01 10:34:30 +0200

    * the launching delegate now starts the transformation

--- plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/launching/GentransLaunchingDelegate.java
+++ plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/launching/GentransLaunchingDelegate.java
@@ -1,21 +1,127 @@
 package de.mfreund.pamtram.launching;
 
+import java.util.Collections;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+import de.mfreund.gentrans.transformation.GenericTransformationRunner;
 
 public class GentransLaunchingDelegate implements ILaunchConfigurationDelegate {
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		System.out.println("launching via the delegate");
-		System.out.println(configuration.getAttribute("project", ""));
-		System.out.println(configuration.getAttribute("srcFile", ""));
-		System.out.println(configuration.getAttribute("pamtramFile", ""));
-		System.out.println(configuration.getAttribute("targetFile", ""));
+		
+		// validate the launch configuration
+		if(!validateLaunchConfig(configuration)) {
+			return;
+		}
+		
+		// get the associated files from the launch configuration
+		String project = configuration.getAttribute("project", "") + Path.SEPARATOR;
+		String sourceFile = project + 
+				"Source" + Path.SEPARATOR + configuration.getAttribute("srcFile", "");
+		String pamtramFile = project + 
+				"Pamtram" + Path.SEPARATOR + configuration.getAttribute("pamtramFile", "");
+		String targetFile = project + 
+				"Target" + Path.SEPARATOR + configuration.getAttribute("targetFile", "");
+		
+		// Create a resource set. 
+		ResourceSet rs = new ResourceSetImpl();
+		
+		// the selected resource (IMPORTANT: needs to be represented as absolute URI with "file://" scheme; 
+		// if other schemes are used, the relative paths to the wprops and other model files are not set correct!)
+		URI sourceUri = URI.createFileURI(new java.io.File(sourceFile).toString());
+
+		Resource sourceResource = null;
+		try {
+			// load the source file
+			sourceResource = rs.getResource(sourceUri, true);
+			sourceResource.load(Collections.EMPTY_MAP);
+		} catch(Exception e) {
+			MessageDialog.openError(new Shell(), "Error loading resource", 
+					e.getMessage());
+			return;
+		}
+		
+		// get the root object of the xml resource
+		EObject root = sourceResource.getContents().get(0);
+		final GenericTransformationRunner tr = 
+				new GenericTransformationRunner(root, pamtramFile, targetFile);
+
+		Job job = new Job("Gentrans"){
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try{
+					// perform the transformation
+					tr.runTransformation();
+					return org.eclipse.core.runtime.Status.OK_STATUS;
+					
+				} catch (Exception e){
+					e.printStackTrace(System.out);
+					return org.eclipse.core.runtime.Status.CANCEL_STATUS;//TODO
+				}
+
+			}
+			
+		};
+		job.schedule();
+		
+	}
+
+	/** 
+	 * Validates a launch configuration by check if all attributes
+	 * have meaningful values.
+	 * 
+	 * @param configuration the launch configuration to validate
+	 * @return true if the launch configuration is valid, false otherwise
+	 */
+	private boolean validateLaunchConfig(ILaunchConfiguration configuration) {
+		try {
+			if(configuration.getAttribute("project", "").equals("")) {
+				MessageDialog.openError(new Shell(), "Error", 
+						"No project has been specified!");
+				return false;
+			}
+			
+			if(configuration.getAttribute("srcFile", "").equals("")) {
+				MessageDialog.openError(new Shell(), "Error", 
+						"No source file has been specified!");
+				return false;
+			}
+		
+			if(configuration.getAttribute("pamtramFile", "").equals("")) {
+				MessageDialog.openError(new Shell(), "Error", 
+						"No pamtram file has been specified!");
+				return false;
+			}
+			
+			if(configuration.getAttribute("targetFile", "").equals("")) {
+				MessageDialog.openError(new Shell(), "Error", 
+						"No target file has been specified!");
+				return false;
+			}
+		} catch (CoreException e) {
+			MessageDialog.openError(new Shell(), "Error", 
+					e.getMessage());
+			return false;
+		}
+		return true;
 	}
 
 }

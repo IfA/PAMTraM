13d1150fb6667c89092665585e33ed8c6fb20354
Sascha Steffen <steffensascha@gmail.com>
2014-08-17 16:41:56 +0200

    rename

---
+++ plugins/de.mfreund.pamtram.model/src/pamtram/mapping/ExternalAttributeMappingSourceElement.java
@@ -0,0 +1,17 @@
+/**
+ */
+package pamtram.mapping;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>External Attribute Mapping Source Element</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see pamtram.mapping.MappingPackage#getExternalAttributeMappingSourceElement()
+ * @model abstract="true"
+ * @generated
+ */
+public interface ExternalAttributeMappingSourceElement extends AttributeMappingSourceElementWithModifiers {
+} // ExternalAttributeMappingSourceElement
---
+++ plugins/de.mfreund.pamtram.model/src/pamtram/mapping/impl/ExternalAttributeMappingSourceElementImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package pamtram.mapping.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import pamtram.mapping.ExternalAttributeMappingSourceElement;
+import pamtram.mapping.MappingPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>External Attribute Mapping Source Element</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class ExternalAttributeMappingSourceElementImpl extends AttributeMappingSourceElementWithModifiersImpl implements ExternalAttributeMappingSourceElement {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ExternalAttributeMappingSourceElementImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MappingPackage.Literals.EXTERNAL_ATTRIBUTE_MAPPING_SOURCE_ELEMENT;
+	}
+
+} //ExternalAttributeMappingSourceElementImpl

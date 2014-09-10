b5e5aebd632d71d9f965bbe4c37f39fd86887a02
Sascha Steffen <steffensascha@gmail.com>
2014-09-10 15:23:30 +0200

    choices for Matchers depend on value of affectedReference

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/AttributeMatcherItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/AttributeMatcherItemProvider.java
@@ -9,15 +9,15 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 
-import pamtram.mapping.AttributeMatcher;
 import pamtram.mapping.MappingInstanceSelector;
 import pamtram.mapping.MappingPackage;
 import pamtram.metamodel.TargetSectionAttribute;
+import pamtram.metamodel.TargetSectionClass;
 
 /**
  * This is the item provider adapter for a {@link pamtram.mapping.AttributeMatcher} object.
@@ -75,25 +75,18 @@ public class AttributeMatcherItemProvider
 
 					@Override
 					public Collection<?> getChoiceOfValues(Object object) {
-						AttributeMatcher matcher=(AttributeMatcher) object;
 						//the parent MappingInstanceSelector
-						MappingInstanceSelector miSelector= (MappingInstanceSelector) matcher.eContainer();
-						EClass refType=miSelector.getAffectedReference().getEReference().getEReferenceType();
-						Collection<?> choices= super.getChoiceOfValues(object);
-						Collection<TargetSectionAttribute> newChoices=new LinkedList<TargetSectionAttribute>();
-						
+						Collection<TargetSectionAttribute> choices=new LinkedList<TargetSectionAttribute>();
+						Collection<TargetSectionClass>refClasses=((MappingInstanceSelector) ((EObject) object).eContainer()).getAffectedReference().getValue();
+
 						/*
-						 * Only show attributes that belong to an Class that fits the Reference type
+						 * Only show attributes that belong to an Class that was modeled as the reference's value
 						 */
-						for(Object choice : choices){
-							if(choice instanceof TargetSectionAttribute){
-								if(((TargetSectionAttribute) choice).getOwningClass().getEClass().isSuperTypeOf(refType)){
-									newChoices.add((TargetSectionAttribute) choice);
-								} 
-							}
+						for(TargetSectionClass val : refClasses){
+							choices.addAll(val.getAttributes());
 						}
 						
-						return newChoices;
+						return choices;
 					}
 				
 				
--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/ClassMatcherItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/ClassMatcherItemProvider.java
@@ -4,13 +4,10 @@ package pamtram.mapping.provider;
 
 
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -18,7 +15,6 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 
 import pamtram.mapping.MappingInstanceSelector;
 import pamtram.mapping.MappingPackage;
-import pamtram.metamodel.TargetSectionClass;
 
 /**
  * This is the item provider adapter for a {@link pamtram.mapping.ClassMatcher} object.
@@ -74,34 +70,12 @@ public class ClassMatcherItemProvider
 				 null,
 				 null){
 
-					@Override
-					public Collection<?> getChoiceOfValues(Object object) {
-						Collection<?> superChoices=super.getChoiceOfValues(object);
-						
-						EClass refClass=null;
-						try{
-							refClass=((MappingInstanceSelector) ((EObject) object).eContainer()).getAffectedReference().getEReference().getEReferenceType();
-						} catch(Exception e){
-							return superChoices;
-						}
-							List<TargetSectionClass> choices=new LinkedList<TargetSectionClass>();
-							Iterator<?>  it=superChoices.iterator();
-							while(it.hasNext()){
-								Object next=it.next();
-								if(next instanceof TargetSectionClass){
-									if(((TargetSectionClass) next).getEClass() != null){
-										if(refClass.isSuperTypeOf(((TargetSectionClass) next).getEClass())){
-											choices.add((TargetSectionClass) next);
-										}
-									}
-								}
-							}
-							
-							return choices;
-						
-					}
-				
-				
+				@Override
+				public Collection<?> getChoiceOfValues(Object object) {
+					return ((MappingInstanceSelector) ((EObject) object).eContainer()).getAffectedReference().getValue();
+				}
+
+
 			});
 	}
 

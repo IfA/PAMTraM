686a806c53c3d5971e8e79d0b2da17773326aa41
Sascha Steffen <steffensascha@gmail.com>
2014-10-12 19:00:39 +0200

    commented CancellationListener

--- plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/CancellationListener.java
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/CancellationListener.java
@@ -1,14 +1,22 @@
 /**
- * 
+ *
  */
 package de.mfreund.gentrans.transformation;
 
 /**
  * @author Sascha Steffen
+ * @version 1.0
  *
+ *          Provides methods to handle a Job cancellation.
  */
 public interface CancellationListener {
+	/**
+	 * Cancel (possibly long running) operations
+	 */
 	public abstract void cancel();
-	
+
+	/**
+	 * @return true if cancel() was called
+	 */
 	public abstract boolean isCancelled();
 }

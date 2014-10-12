45cbb5bd41605e4d6b5ca1eb725f123760087f4d
Sascha Steffen <steffensascha@gmail.com>
2014-10-12 19:27:19 +0200

    cleaned up Pair

--- plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/Pair.java
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/Pair.java
@@ -1,29 +1,73 @@
 package de.mfreund.gentrans.transformation;
-//copied from https://stackoverflow.com/questions/521171/a-java-collection-of-value-pairs-tuples
-public class Pair<L,R> {
 
-	  private final L left;
-	  private final R right;
+/**
+ * Class to hold references to two objects. Can be used by a method two return
+ * two objects.
+ *
+ * @author copied from
+ *         https://stackoverflow.com/questions/521171/a-java-collection
+ *         -of-value-pairs-tuples
+ * @version 1.0
+ * @param <L>
+ *            left type
+ * @param <R>
+ *            right type
+ */
+public class Pair<L, R> {
 
-	  public Pair(L left, R right) {
-	    this.left = left;
-	    this.right = right;
-	  }
+	private final L left;
+	private final R right;
 
-	  public L getLeft() { return left; }
-	  public R getRight() { return right; }
-
-	  @Override
-	  public int hashCode() { return left.hashCode() ^ right.hashCode(); }
-
-	  @Override
-	  public boolean equals(Object o) {
-	    if (o == null) return false;
-	    if (!(o instanceof Pair)) return false;
-	    @SuppressWarnings("rawtypes")
-		Pair pairo = (Pair) o;
-	    return this.left.equals(pairo.getLeft()) &&
-	           this.right.equals(pairo.getRight());
-	  }
-
+	/**
+	 * @param left
+	 *            element
+	 * @param right
+	 *            element
+	 */
+	public Pair(final L left, final R right) {
+		this.left = left;
+		this.right = right;
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object o) {
+		if (o == null)
+			return false;
+		if (!(o instanceof Pair))
+			return false;
+		@SuppressWarnings("rawtypes")
+		final Pair pairo = (Pair) o;
+		return this.left.equals(pairo.getLeft())
+				&& this.right.equals(pairo.getRight());
+	}
+
+	/**
+	 * @return left element
+	 */
+	public L getLeft() {
+		return left;
+	}
+
+	/**
+	 * @return right element
+	 */
+	public R getRight() {
+		return right;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return left.hashCode() ^ right.hashCode();
+	}
+
+}

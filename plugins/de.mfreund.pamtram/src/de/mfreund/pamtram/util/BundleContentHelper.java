4fdc7a31769bbbab49a83ecf174018684cf89bb6
mfreund <matth.freund@gmail.com>
2014-11-26 16:32:08 +0100

    Fixes and improvements for the BundleContentHelper
    Add a method to retrieve an image from a bundle and change the other
    methods to allow the specification of the bundle from which entries
    shall be retrieved.

--- plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/util/BundleContentHelper.java
+++ plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/util/BundleContentHelper.java
@@ -10,18 +10,23 @@ import java.util.Collections;
 import java.util.Enumeration;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
 import org.osgi.framework.Bundle;
 
 public abstract class BundleContentHelper {
 	/**
-	 * Get file from the de.tud.et.ifa.agtele.autoprobe.transformation plugin
+	 * Get a file from a plugin
 	 * 
+	 * @param bundleId The id of the plugin that shall be used to
+	 * 	resolve the path.
 	 * @param path path relative to the bundle root
 	 * @return file from the bundle
 	 */
-	public static File getBundleEntry(String path) {
-		Bundle bundle = Platform.getBundle("de.tud.et.ifa.agtele.autoprobe.transformation");
+	public static File getBundleEntry(String bundleId, String path) {
+		Bundle bundle = Platform.getBundle(bundleId);
 		File f = null;
 		
     	try {
@@ -42,10 +47,12 @@ public abstract class BundleContentHelper {
 	 * Get contents of a folder in the de.tud.et.ifa.agtele.autoprobe.transformation plugin
 	 * 
 	 * @param dir path of the folder relative to the bundle root
+	 * @param bundleId The id of the plugin that shall be used to
+	 * 	resolve the path.
 	 * @return list of files from the bundle folder
 	 */
-	public static ArrayList<File> getBundleContents(String dir) {
-    	Bundle bundle = Platform.getBundle("de.tud.et.ifa.agtele.autoprobe.transformation");
+	public static ArrayList<File> getBundleContents(String bundleId, String dir) {
+    	Bundle bundle = Platform.getBundle(bundleId);
     	ArrayList<File> files = new ArrayList<File>();
     	
     	// get all bundle entries of the folder
@@ -56,18 +63,36 @@ public abstract class BundleContentHelper {
 	    	// iterate over the BundleContents
 	    	for (String file : Collections.list(bundle.getEntryPaths(dir))) {
 	    		// get the file from the bundle
-	    		File f = getBundleEntry(file);
+	    		File f = getBundleEntry(bundleId, file);
 	    		
 	    		// add the file to the file list
 	    		files.add(f);
 	
 	    		// if the current file is a directory then get its entry recursively
 	    		if (f.isDirectory()) {
-	    			files.addAll(getBundleContents(file));
+	    			files.addAll(getBundleContents(bundleId, file));
 	    		}
 			}
     	}
     	return files;
+    }
+	
+	/**
+	 * Get an image from a plugin
+	 * 
+	 * @param bundleId The id of the plugin that shall be used to
+	 * 	resolve the path.
+	 * @param path path relative to the bundle root
+	 * @return image The image resolved from the bundle
+	 */
+	public static Image getBundleImage(String bundleId, String path) {
+		
+		Bundle bundle = Platform.getBundle(bundleId);
+		ImageDescriptor desc = ImageDescriptor.createFromURL(
+	              FileLocator.find(bundle, new Path(path), null));
+		Image image = desc.createImage();
+    	
+    	return image;
     }
 	
 }

b8491b1f7b5553309427f614a479c2354900d00d
mfreund <matth.freund@gmail.com>
2015-02-27 15:25:23 +0100

    Adapt to changes in the model plug-in
    The LibraryHelper now automatically generates the necessary
    'AttributeParamters', 'ContainerParameters' and
    'ExternalReferenceParameter'.

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/util/LibraryHelper.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/util/LibraryHelper.java
@@ -3,34 +3,33 @@ package pamtram.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
 import pamtram.metamodel.ActualAttribute;
-import pamtram.metamodel.CardinalityType;
+import pamtram.metamodel.AttributeParameter;
+import pamtram.metamodel.ContainerParameter;
+import pamtram.metamodel.ExternalReferenceParameter;
 import pamtram.metamodel.LibraryEntry;
+import pamtram.metamodel.LibraryParameter;
 import pamtram.metamodel.MetaModelElement;
-import pamtram.metamodel.NonContainmentReference;
-import pamtram.metamodel.TargetSectionAttribute;
 import pamtram.metamodel.TargetSectionClass;
-import pamtram.metamodel.TargetSectionContainmentReference;
 import pamtram.metamodel.TargetSectionNonContainmentReference;
 import pamtram.metamodel.impl.MetamodelFactoryImpl;
-import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.AbstractAttributeMapper;
-import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.LibraryItem;
-import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.MetaData;
-import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.impl.GenLibraryFactoryImpl;
-import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.impl.GenLibraryPackageImpl;
+import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.AbstractAttributeParameter;
+import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.AbstractContainerParameter;
+import de.tud.et.ifa.agtele.genlibrary.model.genlibrary.AbstractExternalReferenceParameter;
 import de.tud.et.ifa.agtele.genlibrary.util.impl.FileParserImpl;
 import de.tud.et.ifa.agtele.genlibrary.util.interfaces.LibraryFileEntry;
 
@@ -44,27 +43,25 @@ public class LibraryHelper {
 
 	/**
 	 * This method converts a LibraryEntry represented by a given path to the library file and a 
-	 * relative path inside the library file to a pamtram LibraryElement. Therefore, it iterates
-	 * through the library entry and generates TargetSectionClasses, -References and -Attributes
-	 * for every model element it encounters if its type is represented in the target metamodel.
-	 * Consequently, no Classes, References or Attributes are created for the 'library' types like
-	 * 'MetaData', 'Resource' and so on. In order to be able to determine, if an element is part of 
-	 * the target metamodel, the target ePackage has to be passed as parameter.
+	 * relative path inside the library file to a pamtram LibraryEntry. Therefore, it generates {@link LibraryParameter}s 
+	 * for every {@link AbstractAttributeParameter}, {@link AbstractContainerParameter} and 
+	 * {@link AbstractExternalReferenceParameter} that it encounters. 
 	 * 
 	 * @param libraryFile The path to a library file (Zip or Jar).
 	 * @param path The path of the library entry inside the library (e.g. 'my.path').
 	 * @param targetEPackage The target ePackage of the pamtram model into that the generated library
 	 * element shall be inserted later on.
+	 * @param uri The URI where the resource created for the library element shall be stored.
 	 * @return The generated LibraryEntry.
 	 * @throws IOException If something goes wrong during loading the libraryFile or the XMI file inside
 	 * the library file representing the LibraryEntry. 
 	 */
 	public static LibraryEntry convertToLibraryElement(String libraryFile, String path,
-			EPackage targetEPackage) throws IOException {
+			EPackage targetEPackage, URI uri) throws IOException {
 		
 		//TODO maybe use a factory pattern for this?
 		LibraryElementConverter converter =
-				(new LibraryHelper()).new LibraryElementConverter(libraryFile, path, targetEPackage);
+				(new LibraryHelper()).new LibraryElementConverter(libraryFile, path, targetEPackage, uri);
 		
 		return converter.convert();
 	}
@@ -167,18 +164,11 @@ public class LibraryHelper {
 		 * This is the generated LibraryEntry.
 		 */
 		private LibraryEntry pamtramLibEntry;
-		
-		/**
-		 * This map keeps track of the metamodel elements that have been created for the
-		 * library entries.
-		 */
-		private HashMap<EObject, MetaModelElement> libEntries2metaModelElementsMap = new HashMap<>();
 
 		/**
-		 * This map keeps track of the non-containment references for that target values have to be found after
-		 * iterating through the containment tree.
+		 * The URI where the resource created for the library element shall be stored.
 		 */
-		private HashMap<TargetSectionNonContainmentReference, EObject> nonContainmentTargetMap = new HashMap<>();
+		private URI uri;
 		
 		/**
 		 * This constructs an instance of the LibraryElementConverter.
@@ -187,16 +177,18 @@ public class LibraryHelper {
 		 * @param path The path of the library entry inside the library (e.g. 'my.path').
 		 * @param targetEPackage The target ePackage of the pamtram model into that the generated library
 		 * element shall be inserted later on.
+		 * @param uri The URI where the resource created for the library element shall be stored.
 		 */
 		public LibraryElementConverter(String libraryFile, String path,
-			EPackage targetEPackage) {
+			EPackage targetEPackage, URI uri) {
 			this.libraryFile = libraryFile;
 			this.path = path;
 			this.targetEPackages = EPackageHelper.collectEPackages(targetEPackage);
+			this.uri = uri;
 		}
 		
 		/**
-		 * This performs the actual conversion from the LibraryFileEntry to a LibraryElement. 
+		 * This performs the actual conversion from the {@link LibraryFileEntry} to a {@link LibraryEntry}. 
 		 * @return The generated LibraryElement.
 		 * @throws IOException 
 		 */
@@ -206,6 +198,8 @@ public class LibraryHelper {
 			libFileEntry = getLibraryFileEntry(libraryFile, path);
 			libEntry = getLibraryEntry(libFileEntry);
 			
+			storeLibraryEntry(libEntry, uri.appendSegment(path).appendSegment("data.xmi"));
+			
 			// create the LibraryElement to be returned
 			pamtramLibEntry = MetamodelFactoryImpl.eINSTANCE.createLibraryEntry();
 			
@@ -214,164 +208,90 @@ public class LibraryHelper {
 			pamtramLibEntry.setPath(libFileEntry.getKey());
 			pamtramLibEntry.setLibraryFile(libraryFile);
 			
-//			if(libEntry.getMetaData() == null) {
-//				return pamtramLibEntry;
-//			}
-//			
-//			for (AbstractAttributeMapper<EObject> attMapper : libEntry.getMetaData().getAttributeMappers()) {
-//				
-//				TargetSectionClass target = MetamodelFactoryImpl.eINSTANCE.createTargetSectionClass();
-//				target.setEClass(attMapper.eClass());
-//				
-//				TargetSectionNonContainmentReference source = MetamodelFactoryImpl.eINSTANCE.createTargetSectionNonContainmentReference();
-//				source.setEReference(GenLibraryPackageImpl.eINSTANCE.getAbstractAttributeMapper_Source());
-//				source.getValue().add(attMapper.getSource());
-//				target.getReferences().add(source);
-//				
-//				ActualAttribute attribute = MetamodelFactoryImpl.eINSTANCE.createActualAttribute();
-//				attribute.setAttribute(attMapper.getAttribute());
-//				target.getAttributes().add(attribute);
-//				
-//				pamtramLibEntry.getParameters().add(target);
-//				
-//			}
+			pamtramLibEntry.setOriginalLibraryEntry(libEntry);
 			
-			// now, iterate over the contents of the LibraryEntry
-			populateLibraryElement(pamtramLibEntry, libEntry);
+			if(libEntry.getParameterDescription() == null) {
+				return pamtramLibEntry;
+			}
 			
-			// finally, try to set the non-containment references
-			findNonContainmentReferenceTargets();
+			/*
+			 * Create and populate a Pamtram AttributeParameter for every Genlib AttributeParameter
+			 */
+			for (AbstractAttributeParameter<EObject> attParameter : libEntry.getParameterDescription().getAttributeParameters()) {
+				
+				AttributeParameter param = MetamodelFactoryImpl.eINSTANCE.createAttributeParameter();
+				param.setSource(attParameter.getSource());
+				
+				ActualAttribute attribute = MetamodelFactoryImpl.eINSTANCE.createActualAttribute();
+				attribute.setAttribute(attParameter.getAttribute());
+				attribute.setName(attParameter.getAttribute().getName());
+				
+				param.setAttribute(attribute);
+				
+				pamtramLibEntry.getParameters().add(param);
+				
+			}
+
+			/*
+			 * Create and populate a Pamtram ContainerParameter for every Genlib ContainerParameter
+			 */
+			for (AbstractContainerParameter<EObject, EObject> contParameter : libEntry.getParameterDescription().getContainerParameters()) {
+				
+				ContainerParameter param = MetamodelFactoryImpl.eINSTANCE.createContainerParameter();
+				param.setSource(contParameter.getSource());
+				
+				TargetSectionClass clazz = MetamodelFactoryImpl.eINSTANCE.createTargetSectionClass();
+				clazz.setEClass(contParameter.getSource().eClass());
+				clazz.setName(contParameter.getSource().eClass().getName());
+				
+				param.setClass(clazz);
+				
+				pamtramLibEntry.getParameters().add(param);
+				
+			}
+			
+			/*
+			 * Create and populate a Pamtram ExternalReferenceParameter for every Genlib ExternalReferenceParameter
+			 */
+			for (AbstractExternalReferenceParameter<EObject, EObject> extRefParameter : libEntry.getParameterDescription().getExternalReferenceParameters()) {
+				
+				EObject source = extRefParameter.getSource();
+
+				// search for cross references to the old source object from
+				// the library item
+				Collection<Setting> crossReferences = EcoreUtil.UsageCrossReferencer.find(source, libEntry);
+
+				// create a paramter for every setting
+				for (Setting setting : crossReferences) {
+					ExternalReferenceParameter param = MetamodelFactoryImpl.eINSTANCE.createExternalReferenceParameter();
+					
+					param.setSource(setting.getEObject());
+					
+					TargetSectionNonContainmentReference ref = MetamodelFactoryImpl.eINSTANCE.createTargetSectionNonContainmentReference();
+					ref.setEReference((EReference) setting.getEStructuralFeature());
+					ref.setName(setting.getEStructuralFeature().getName());
+					
+					param.setReference(ref);
+					
+					pamtramLibEntry.getParameters().add(param);
+				}
+			}
 			
 			return pamtramLibEntry;
 		}
 		
-		/**
-		 * This function is recursively called to iterate through the LibraryEntry and generate the
-		 * corresponding LibraryElement. Targets of non-containment references thereby have to be set
-		 * manually afterwards by calling 'findNonContainmentReferenceTargets()'.
-		 * @param parentObject The object that shall serve as the container for newly created elements.
-		 * At the beginning, this will be the LibraryElement itself, later on this will be a 
-		 * TargetSectionContainmentReference.
-		 * @param currentObject A pointer to the part of the LibraryElement that we are currently
-		 * browsing through.
-		 */
-		private void populateLibraryElement(EObject parentObject, EObject currentObject) {
+		private void storeLibraryEntry(de.tud.et.ifa.agtele.genlibrary.model.genlibrary.LibraryEntry libEntry, URI uri) throws IOException {
 			
-			// do not iterate through the metadata of a LibraryItem so that no elements are generated for it
-			if(currentObject instanceof MetaData) {
-//				return;
+			if(libEntry == null) {
+				return;
 			}
-			
-			
-			// only generate metamodel elements for elements that do belong to the target namespace
-			if(currentObject instanceof de.tud.et.ifa.agtele.genlibrary.model.genlibrary.LibraryEntry || 
-					currentObject instanceof LibraryItem || currentObject instanceof MetaData) {
-				// browse deeper in the containment hierarchy
-				for (EObject child : currentObject.eContents()) {
-					populateLibraryElement(currentObject, child);
-				}
-			}
-//			else if(targetEPackages.contains(currentObject.eClass().getEPackage())) {
-			else {
-				// first generate a target section class for the element itself and
-				// attach it to the parentObject
-				TargetSectionClass tClass = 
-						MetamodelFactoryImpl.eINSTANCE.createTargetSectionClass();
-				tClass.setEClass(currentObject.eClass());
-				tClass.setName(currentObject.eClass().getName());
-				tClass.setCardinality(CardinalityType.ONE);
-				libEntries2metaModelElementsMap.put(currentObject, tClass);
-				if(parentObject instanceof LibraryItem) {
-					pamtramLibEntry.getLibraryItem().add(tClass);
-				} else if(parentObject instanceof MetaData) {
-					pamtramLibEntry.getMetaData().add(tClass);
-				} else if(parentObject instanceof TargetSectionContainmentReference) {
-					((TargetSectionContainmentReference) parentObject).getValue().add(tClass);
-				} else {
-					throw new RuntimeException("Internal Error");
-				}
-				
-				// second, generate a target section attribute for every attribute
-				for (EAttribute att : currentObject.eClass().getEAllAttributes()) {
-					if(currentObject.eGet(att) != null) {
-						ActualAttribute tAttribute = 
-								MetamodelFactoryImpl.eINSTANCE.createActualAttribute();
-						tAttribute.setAttribute(att);
-						tAttribute.setName(att.getName());
-						// pimp the name of the surrounding Class
-						if(att.getName().equalsIgnoreCase("name")) {
-							tClass.setName(tClass.getName() + " " + currentObject.eGet(att)); 
-						}
-						tAttribute.setUnique(false);
-						tAttribute.setValue(String.valueOf(currentObject.eGet(att)));
-						tClass.getAttributes().add(tAttribute);
-					}
-				}
-				
-				// now, iterate through all containment references
-				for (EReference ref : currentObject.eClass().getEAllContainments()) {
-					if(currentObject.eGet(ref) != null) {
-						TargetSectionContainmentReference tReference = 
-								MetamodelFactoryImpl.eINSTANCE.createTargetSectionContainmentReference();
-						tReference.setEReference(ref);
-						tReference.setName(ref.getName());
-						tClass.getReferences().add(tReference);
-						
-						// browse deeper in the containment hierarchy
-						if(!ref.isMany()) {
-							populateLibraryElement(tReference, (EObject) currentObject.eGet(ref));
-						} else {
-							for (Object child : (EList<?>) currentObject.eGet(ref)) {
-								populateLibraryElement(tReference, (EObject) child);
-							}
-						}
-					}
-				}
-				// at last, we can try to set the non-containment references because all objects should
-				// already have been created (we iterated through the containment references first!)
-				for (EReference ref : currentObject.eClass().getEAllReferences()) {
-					if(ref.isContainment() || currentObject.eGet(ref) == null) {
-						continue;
-					}
-					
-					TargetSectionNonContainmentReference tReference = 
-							MetamodelFactoryImpl.eINSTANCE.createTargetSectionNonContainmentReference();
-					tReference.setEReference(ref);
-					tReference.setName(ref.getName());
-					tClass.getReferences().add(tReference);
-					// try to set the target of the reference
-					if(!ref.isMany()) {
-						nonContainmentTargetMap.put(tReference, (EObject) currentObject.eGet(ref));
-					} else {
-						for (Object child : (EList<?>) currentObject.eGet(ref)) {
-							nonContainmentTargetMap.put(tReference, (EObject) child);
-						}
-					}
-				}
-//			} else {
-//				// browse deeper in the containment hierarchy
-//				for (EObject child : currentObject.eContents()) {
-//					populateLibraryElement(parentObject, child);
-//				}
-			}
-			
-		}
 		
-		/**
-		 * This iterates over the nonContainmentTargetMap and tries to set the
-		 * target value for each non containment reference.
-		 */
-		private void findNonContainmentReferenceTargets() {
+			// create a resource and add the library entry
+			Resource resource = new XMIResourceImpl(uri);
+			resource.getContents().add(libEntry);
 			
-			for (TargetSectionNonContainmentReference ref : nonContainmentTargetMap.keySet()) {
-				EObject target = 
-						libEntries2metaModelElementsMap.get(nonContainmentTargetMap.get(ref));
-				if(target != null && target instanceof TargetSectionClass) {
-					ref.getValue().add((TargetSectionClass) target);
-				} else {
-					System.out.println("No target found for non-containment reference " + ref.getName());
-				}
-			}
+			// try to save the resource
+			resource.save(null);
 		}
 	}
 	

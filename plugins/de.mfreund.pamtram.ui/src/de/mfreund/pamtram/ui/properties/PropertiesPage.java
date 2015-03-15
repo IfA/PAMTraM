1a90e2d490b3563a3af76d3ca9d71cd6f4a65a8b
mfreund <matth.freund@gmail.com>
2015-03-15 15:53:01 +0100

    Create a (still empty) properties page for PAMTraM projects

---
+++ plugins/de.mfreund.pamtram.ui/src/de/mfreund/pamtram/ui/properties/PropertiesPage.java
@@ -0,0 +1,19 @@
+package de.mfreund.pamtram.ui.properties;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class PropertiesPage extends PropertyPage implements
+		IWorkbenchPropertyPage {
+
+	public PropertiesPage() {
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		return null;
+	}
+
+}

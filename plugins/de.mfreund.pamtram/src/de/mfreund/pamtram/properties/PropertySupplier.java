69599545e860536300fc56695cc871b6f9bcf3ec
mfreund <matth.freund@gmail.com>
2015-03-19 16:10:17 +0100

    Add convenience method 'getResourceProperty' to the PropertySupplier

--- plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/properties/PropertySupplier.java
+++ plugins/de.mfreund.pamtram/src/de/mfreund/pamtram/properties/PropertySupplier.java
@@ -1,5 +1,9 @@
 package de.mfreund.pamtram.properties;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+
 
 /**
  * This class is responsible for the definition of properties for the PAMTraM plug-ins and default 
@@ -37,5 +41,43 @@ public class PropertySupplier {
 	 * This is the id of the bundle that identifies the properties.
 	 */
 	public static final String PROPERTY_QUALIFIER = "de.mfreund.pamtram";
+	
+	/**
+	 * This returns the value of the persistent property identified by the given <b>propertyName</b> 
+	 * for a given <b>resource</b>. If the property has not set for the resource previously, the default 
+	 * value for the property is returned.
+	 * 
+	 * @param propertyName The identifier of the property to be retrieved (one of the static final fields 
+	 * of this class starting with <em>PROP_</em>).
+	 * @param resource The resource for which the property shall be retrieved (must not be <em>null</em>).
+	 * @return The value of the property, the default property value if the value has not been set before or
+	 * <em>null</em> if an unknown propertyName has been passed as argument.
+	 * @throws CoreException if this method fails
+	 */
+	public static String getResourceProperty(String propertyName, IResource resource) throws CoreException {
+		
+		String propertyValue = resource.getPersistentProperty(
+				new QualifiedName(PROPERTY_QUALIFIER, propertyName));
+		
+		if(propertyValue != null) {
+			return propertyValue;
+		}
+		
+		// return the default value if necessary
+		switch (propertyName) {
+		case PROP_LIBRARY_TARGET_BUNDLE:
+			propertyValue = DEFAULT_LIBRARY_TARGET_BUNDLE;
+			break;
+		case PROP_LIBRARY_TARGET_CONTEXT:
+			propertyValue = DEFAULT_LIBRARY_TARGET_CONTEXT;
+			break;
+		case PROP_LIBRARY_TARGET_PARSER:
+			propertyValue = DEFAULT_LIBRARY_TARGET_PARSER;
+			break;
+		default:
+			break;
+		}
+		return propertyValue;
+	}
 	
 }

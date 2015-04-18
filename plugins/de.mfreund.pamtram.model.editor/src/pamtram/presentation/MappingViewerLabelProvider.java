60c2bbd6e66ae730ac0b6caaffe14d08afa066a7
mfreund <matth.freund@gmail.com>
2015-04-18 17:21:44 +0200

    Add documentation to the MappingViewerLabelProvider

--- plugins/de.mfreund.pamtram.model.editor/src/pamtram/presentation/MappingViewerLabelProvider.java
+++ plugins/de.mfreund.pamtram.model.editor/src/pamtram/presentation/MappingViewerLabelProvider.java
@@ -10,6 +10,14 @@ import org.eclipse.swt.widgets.Display;
 
 import pamtram.DeactivatableElement;
 
+/**
+ * This is a special LabelProvider that takes {@link DeactivatableElement}s into account
+ * and adjusts the icon and the foreground color if {@link DeactivatableElement#isDeactivated()}
+ * returns '<b><em>true</em></b>'.
+ * 
+ * @author mfreund
+ *
+ */
 public class MappingViewerLabelProvider extends AdapterFactoryLabelProvider
 		implements IColorProvider {
 
@@ -17,6 +25,12 @@ public class MappingViewerLabelProvider extends AdapterFactoryLabelProvider
 		super(adapterFactory);
 	}
 
+	/**
+	 *  This implementation checks if the element itself or one of its parents
+	 *  is of type {@link DeactivatableElement} and {@link DeactivatableElement#isDeactivated()}
+	 *  returns '<b><em>true</em></b>'. If this is the case, a color that represents a
+	 *  'grayed out' status is returned. Otherwise, the default color is returned.
+	 */
 	@Override
 	public Color getForeground(Object object) {
 		
@@ -36,7 +50,7 @@ public class MappingViewerLabelProvider extends AdapterFactoryLabelProvider
 			
 		} while (element != null);
 		
-		return super.getBackground(object);
+		return super.getForeground(object);
 	}
 	
 }

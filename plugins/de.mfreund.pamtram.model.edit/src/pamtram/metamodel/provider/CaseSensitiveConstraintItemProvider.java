ca8dd4acb956e725ca1e1a2761d63aee587bb672
mfreund <matth.freund@gmail.com>
2015-07-02 09:19:56 +0200

    Regenerate with new EMF version

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/metamodel/provider/CaseSensitiveConstraintItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/metamodel/provider/CaseSensitiveConstraintItemProvider.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
@@ -15,6 +16,8 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
 
 import pamtram.metamodel.CaseSensitiveConstraint;
 import pamtram.metamodel.MetamodelPackage;
+import pamtram.provider.NamedElementItemProvider;
+import pamtram.provider.PamtramEditPlugin;
 
 /**
  * This is the item provider adapter for a {@link pamtram.metamodel.CaseSensitiveConstraint} object.
@@ -23,7 +26,7 @@ import pamtram.metamodel.MetamodelPackage;
  * @generated
  */
 public class CaseSensitiveConstraintItemProvider
-	extends AttributeValueConstraintItemProvider {
+	extends NamedElementItemProvider {
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc -->
@@ -45,9 +48,55 @@ public class CaseSensitiveConstraintItemProvider
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
+			addTypePropertyDescriptor(object);
+			addValuePropertyDescriptor(object);
 			addCaseSensitivePropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Type feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addTypePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_AttributeValueConstraint_type_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_AttributeValueConstraint_type_feature", "_UI_AttributeValueConstraint_type"),
+				 MetamodelPackage.Literals.ATTRIBUTE_VALUE_CONSTRAINT__TYPE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Value feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addValuePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_AttributeValueConstraint_value_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_AttributeValueConstraint_value_feature", "_UI_AttributeValueConstraint_type"),
+				 MetamodelPackage.Literals.ATTRIBUTE_VALUE_CONSTRAINT__VALUE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
 	}
 
 	/**
@@ -98,6 +147,8 @@ public class CaseSensitiveConstraintItemProvider
 		updateChildren(notification);
 
 		switch (notification.getFeatureID(CaseSensitiveConstraint.class)) {
+			case MetamodelPackage.CASE_SENSITIVE_CONSTRAINT__TYPE:
+			case MetamodelPackage.CASE_SENSITIVE_CONSTRAINT__VALUE:
 			case MetamodelPackage.CASE_SENSITIVE_CONSTRAINT__CASE_SENSITIVE:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
@@ -115,6 +166,17 @@ public class CaseSensitiveConstraintItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return PamtramEditPlugin.INSTANCE;
 	}
 
 }
--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/metamodel/provider/RegExMatcherItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/metamodel/provider/RegExMatcherItemProvider.java
@@ -8,9 +8,16 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import pamtram.metamodel.MetamodelPackage;
 import pamtram.metamodel.RegExMatcher;
+import pamtram.provider.NamedElementItemProvider;
+import pamtram.provider.PamtramEditPlugin;
 
 /**
  * This is the item provider adapter for a {@link pamtram.metamodel.RegExMatcher} object.
@@ -19,7 +26,7 @@ import pamtram.metamodel.RegExMatcher;
  * @generated
  */
 public class RegExMatcherItemProvider
-	extends AttributeValueConstraintItemProvider {
+	extends NamedElementItemProvider {
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc -->
@@ -41,8 +48,54 @@ public class RegExMatcherItemProvider
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
+			addTypePropertyDescriptor(object);
+			addValuePropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Type feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addTypePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_AttributeValueConstraint_type_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_AttributeValueConstraint_type_feature", "_UI_AttributeValueConstraint_type"),
+				 MetamodelPackage.Literals.ATTRIBUTE_VALUE_CONSTRAINT__TYPE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Value feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addValuePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_AttributeValueConstraint_value_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_AttributeValueConstraint_value_feature", "_UI_AttributeValueConstraint_type"),
+				 MetamodelPackage.Literals.ATTRIBUTE_VALUE_CONSTRAINT__VALUE,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
 	}
 
 	/**
@@ -80,6 +133,13 @@ public class RegExMatcherItemProvider
 	@Override
 	public void notifyChanged(Notification notification) {
 		updateChildren(notification);
+
+		switch (notification.getFeatureID(RegExMatcher.class)) {
+			case MetamodelPackage.REG_EX_MATCHER__TYPE:
+			case MetamodelPackage.REG_EX_MATCHER__VALUE:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
 		super.notifyChanged(notification);
 	}
 
@@ -93,6 +153,17 @@ public class RegExMatcherItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getResourceLocator() {
+		return PamtramEditPlugin.INSTANCE;
 	}
 
 }

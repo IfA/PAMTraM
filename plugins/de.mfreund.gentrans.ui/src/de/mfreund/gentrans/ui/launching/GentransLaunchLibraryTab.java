82d3c39541ab1162f6d224ef0b86b75c7d46f522
mfreund <matth.freund@gmail.com>
2015-07-26 20:29:11 +0200

    Make library related entries optional in library tab of launch config
    Those are not necessary if no library entries are used. Consequently,
    the transformation should be possible without the specification of all
    the library-related parameters.

--- plugins/de.mfreund.gentrans.ui/src/de/mfreund/gentrans/ui/launching/GentransLaunchLibraryTab.java
+++ plugins/de.mfreund.gentrans.ui/src/de/mfreund/gentrans/ui/launching/GentransLaunchLibraryTab.java
@@ -33,17 +33,17 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 	 * A text field to specify the full path to the folder that holds the target library.
 	 */
 	private Text targetPathText;
-	
+
 	/**
 	 * A text field to specify the bundle that represents the specific target library.
 	 */
 	private Text targetBundleText;
-	
+
 	/**
 	 * A text field to specify the concrete library context of the specific target library.
 	 */
 	private Text targetLibContextText;
-	
+
 	/**
 	 * A text field to specify the concrete library path parser of the specific target library.
 	 */
@@ -51,82 +51,82 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 
 	@Override
 	public void createControl(Composite parent) {
-		
+
 		// a composite to host all other controls
 		Composite comp = new Composite(parent, SWT.NONE);
 		setControl(comp);
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(getControl(), getHelpContextId());
 		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(true).applyTo(comp);
 		comp.setFont(parent.getFont());
-		
+
 		// a group to host all target related settings
 		Group targetGroup = new Group(comp, SWT.NONE);
 		targetGroup.setText("Target");
 		GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).span(2, 1).applyTo(targetGroup);
 		GridLayoutFactory.swtDefaults().numColumns(2).applyTo(targetGroup);
-		
+
 		// create a label for the specification of the path to the folder that holds the target library 
 		Label targetPathLabel = new Label(targetGroup, SWT.NONE);
 		targetPathLabel.setText("Library folder path:");
 		GridDataFactory.swtDefaults().applyTo(targetPathLabel);
-		
+
 		// create a text field for the specification of the path to the folder that holds the target library
 		targetPathText = new Text(targetGroup, SWT.NONE);
 		targetPathText.setMessage("Full path to the folder that holds the target library");
 		GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).applyTo(targetPathText);
 		targetPathText.addModifyListener(new ModifyListener() {
-			
+
 			@Override
 			public void modifyText(ModifyEvent e) {
 				updateLaunchConfigurationDialog();
 			}
 		});
-		
+
 		// create a label for the specification of the bundle that represents the specific target library 
 		Label targetBundleLabel = new Label(targetGroup, SWT.NONE);
 		targetBundleLabel.setText("Bundle ID:");
 		GridDataFactory.swtDefaults().applyTo(targetBundleLabel);
-		
+
 		// create a text field for the specification of the bundle that represents the specific target library 
 		targetBundleText = new Text(targetGroup, SWT.NONE);
 		targetBundleText.setMessage("Bundle identifier of the plug-in hosting the concrete LibraryContext and PathParser");
 		GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).applyTo(targetBundleText);
 		targetBundleText.addModifyListener(new ModifyListener() {
-			
+
 			@Override
 			public void modifyText(ModifyEvent e) {
 				updateLaunchConfigurationDialog();
 			}
 		});
-		
+
 		// create a label for the specification of the concrete library context of the specific target library
 		Label targetLibContextLabel = new Label(targetGroup, SWT.NONE);
 		targetLibContextLabel.setText("Library Context ID:");
 		GridDataFactory.swtDefaults().applyTo(targetLibContextLabel);
-		
+
 		// create a text field for the specification of the concrete library context of the specific target library
 		targetLibContextText = new Text(targetGroup, SWT.NONE);
 		targetLibContextText.setMessage("Fully qualified name of the concrete LibraryContext");
 		GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).applyTo(targetLibContextText);
 		targetLibContextText.addModifyListener(new ModifyListener() {
-			
+
 			@Override
 			public void modifyText(ModifyEvent e) {
 				updateLaunchConfigurationDialog();
 			}
 		});
-		
+
 		// create a label for the specification of the concrete library path parser of the specific target library
 		Label targetLibParserLabel = new Label(targetGroup, SWT.NONE);
 		targetLibParserLabel.setText("Library Path Parser ID:");
 		GridDataFactory.swtDefaults().applyTo(targetLibParserLabel);
-		
+
 		// create a text field for the specification of the concrete library path parser of the specific target library 
 		targetLibParserText = new Text(targetGroup, SWT.NONE);
 		targetLibParserText.setMessage("Fully qualified name of the concrete PathParser (Leave empty to use default parser...)");
 		GridDataFactory.swtDefaults().grab(true, false).align(SWT.FILL, SWT.BEGINNING).applyTo(targetLibParserText);
 		targetLibParserText.addModifyListener(new ModifyListener() {
-			
+
 			@Override
 			public void modifyText(ModifyEvent e) {
 				updateLaunchConfigurationDialog();
@@ -136,17 +136,17 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 
 	@Override
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
-		
+
 		// get the current selection
 		IWorkbench wb = PlatformUI.getWorkbench(); 
 		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
 		ISelection sel = win.getSelectionService().getSelection(); 
-		
+
 		// nothing can be done if the user did not select anything
 		if(sel.isEmpty() || !(sel instanceof TreeSelection)) {
 			return;
 		}
-		
+
 
 		try {
 			// initialize the launch configuration
@@ -188,14 +188,15 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 	public String getName() {
 		return "Library";
 	}
-	
+
 	@Override
 	public boolean isValid(ILaunchConfiguration launchConfig) {
 		setErrorMessage(null);
+		setWarningMessage(null);
 		String targetLibPath = targetPathText.getText();
 		if(targetLibPath.isEmpty()) {
-			setErrorMessage("No target library path has been specified!");
-			return false;
+			setWarningMessage("No target library path has been specified!");
+			return true;
 		} else if(!(new File(targetLibPath)).exists()) {
 			setErrorMessage("Target library path does not exist!");
 			return false;
@@ -213,8 +214,7 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 				}
 				String targetLibContext = launchConfig.getAttribute("targetLibContext", "");
 				if(targetLibContext.isEmpty()) {
-					setErrorMessage("No target library context has been specified!");
-					return false;
+					setWarningMessage("No target library context has been specified!");
 				} else {
 					try {
 						bundle.loadClass(targetLibContext);
@@ -233,7 +233,7 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 					}
 				}
 			} else {
-				return false;
+				setWarningMessage("No bundle for the library context and path parser has been specified");
 			}
 		} catch(Exception e) {
 			setErrorMessage(e.getMessage());
@@ -241,7 +241,7 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 		}
 		return true;
 	}
-	
+
 	/** Initializes the values of a launch configuration based on the current selection
 	 * 
 	 * @param workingCopy a launch configuration to be initialized
@@ -249,10 +249,10 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 	 */
 	private void initLaunchConfiguration(ILaunchConfigurationWorkingCopy workingCopy, 
 			TreeSelection selection) throws CoreException {
-		
+
 		// the selected element
 		Object el = selection.getFirstElement();
-		
+
 		IProject project;
 
 		// determine the project based on the selection
@@ -263,15 +263,15 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 		} else {
 			return;
 		}
-		
+
 		// check if the project has the pamtram nature assigned
 		if(project.hasNature("de.mfreund.pamtram.pamtramNature")) {
-			
+
 			// set the target lib path
 			workingCopy.setAttribute(
 					"targetLibPath",
 					PropertySupplier.getResourceProperty(PropertySupplier.PROP_LIBRARY_TARGET_PATH, project));
-			
+
 			// set the target bundle
 			workingCopy.setAttribute(
 					"targetLibBundle", 
@@ -284,11 +284,11 @@ public class GentransLaunchLibraryTab extends AbstractLaunchConfigurationTab {
 			workingCopy.setAttribute(
 					"targetLibParser",
 					PropertySupplier.getResourceProperty(PropertySupplier.PROP_LIBRARY_TARGET_PARSER, project));
-			
+
 		} else {
 			return;
 		}
-			
+
 	}
 
 }

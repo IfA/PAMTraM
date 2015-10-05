f168ff2e61558b8712d33b6a6d488e0ceae3a7d4
mfreund <matth.freund@gmail.com>
2015-10-05 09:30:09 +0200

    Prevent ConcurrentModificationException in the EPackageSpecificationPage

--- plugins/de.mfreund.pamtram.ui/src/de/mfreund/pamtram/pages/PamtramEPackageSpecificationPage.java
+++ plugins/de.mfreund.pamtram.ui/src/de/mfreund/pamtram/pages/PamtramEPackageSpecificationPage.java
@@ -31,38 +31,36 @@ import pamtram.util.EPackageHelper;
  */
 public class PamtramEPackageSpecificationPage extends WizardPage {
 
-	public PamtramEPackageSpecificationPage(String pageName) {
-		super(pageName);
-	}
-	
 	private final int numColumns = 3;
 	private final int marginWidth = 5;
 	private final int marginHeight = 5;
-	
+
 	private EPackage sourceEPackage, targetEPackage;
 	private Button sourceIsFileBasedButton, targetIsFileBasedButton;
 	private FileFieldEditor sourceFileFieldEditor, targetFileFieldEditor;
 	private Combo sourceCombo, targetCombo;
 	private Composite sourceFileComposite, targetFileComposite;
-	
+
 	// this holds the nsUris of all packages in the ePackage registry
-	private HashMap<String, EPackage> registeredEPackages = new HashMap<>();
+	private HashMap<String, Object> registeredEPackages = new HashMap<>();
 	// this holds the nsUris of all packages defined in the source ecore model (if specified)
 	private HashMap<String, EPackage> sourceEcoreEPackages = new HashMap<>();
 	// this holds the nsUris of all packages defined in the target ecore model (if specified)
 	private HashMap<String, EPackage> targetEcoreEPackages = new HashMap<>();
-	
+
 	private ModifyListener targetComboModifyListener, sourceComboModifyListener;
 	private SelectionListener sourceIsFileBasedButtonSelectionListener, targetIsFileBasedButtonSelectionListener;
 
+	public PamtramEPackageSpecificationPage(String pageName) {
+
+		super(pageName);
+
+		registeredEPackages = new HashMap<>(EPackage.Registry.INSTANCE);
+	}
+
 	@Override
 	public void createControl(Composite parent) {
-		
-		// retrieve the packages from the registry
-		for (String nsUri : EPackage.Registry.INSTANCE.keySet()) {
-			registeredEPackages.put(nsUri, EPackage.Registry.INSTANCE.getEPackage(nsUri));
-		}
-		
+
 		Composite composite = new Composite(parent, SWT.NONE);
 		{
 			GridLayout layout = new GridLayout();
@@ -76,11 +74,11 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			data.horizontalAlignment = GridData.FILL;
 			composite.setLayoutData(data);
 		}
-		
+
 		Group sourceGroup = new Group(composite, SWT.NONE);
 		{
 			sourceGroup.setText("Source Sections ePackage");
-			
+
 			GridLayout layout = new GridLayout();
 			layout.numColumns = 1;
 			layout.marginWidth = marginWidth;
@@ -89,7 +87,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 
 			sourceGroup.setLayoutData(new GridData());
 		}
-		
+
 		final Composite sourceComposite = new Composite(sourceGroup, SWT.NONE);
 		{
 			GridLayout layout = new GridLayout();
@@ -99,29 +97,29 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 
 			sourceComposite.setLayoutData(new GridData());
 		}
-		
+
 		// a checkbox to specify if the source ePackage shall be specified by an ecore model
 		sourceIsFileBasedButton = new Button(sourceComposite, SWT.CHECK);
 		{
-			
+
 			GridData data = new GridData();
 			data.horizontalAlignment = GridData.FILL;
 			data.horizontalSpan = numColumns;
 			sourceIsFileBasedButton.setLayoutData(data);
 		}
-		
+
 		sourceIsFileBasedButton.setText("Source ePackage is ecore-based");
 		sourceIsFileBasedButtonSelectionListener = new SelectionListener() {
-			
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {		
 				setSourceIsFileBased(sourceIsFileBasedButton.getSelection());
 			}
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {}
 		};
-		
+
 		// This is necessary to allow indent for the file field editor
 		sourceFileComposite = new Composite(sourceComposite, SWT.NONE);
 		{			
@@ -136,19 +134,19 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			data.horizontalAlignment = GridData.FILL;
 			sourceFileComposite.setLayoutData(data);
 		}
-			
+
 		// a file field editor to specify the source ecore model
 		sourceFileFieldEditor = new FileFieldEditor("srcEcoreSelect", "", sourceFileComposite);
 		sourceFileFieldEditor.setLabelText("Ecore Model:");
 		sourceFileFieldEditor.setFileExtensions(new String[]{"*.ecore"});
 		sourceFileFieldEditor.setEnabled(false, sourceFileComposite);
 		sourceFileFieldEditor.setPropertyChangeListener(new IPropertyChangeListener() {
-			
+
 			@Override
 			public void propertyChange(PropertyChangeEvent event) {
-				
+
 				HashMap<String, EPackage> packages = EPackageHelper.getEPackages(sourceFileFieldEditor.getStringValue(), true, true);
-				sourceEcoreEPackages = (packages == null ? new HashMap<String, EPackage>() : packages);
+				sourceEcoreEPackages = (packages == null ? new HashMap<>() : packages);
 				if(packages == null) {
 					setErrorMessage("Error while analyzing ePackages in the source ecore model.");
 					sourceCombo.removeAll();
@@ -158,7 +156,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 				}
 			}
 		});
-		
+
 		// a combo box to select the source ePackage
 		sourceCombo = new Combo(sourceComposite, SWT.BORDER);
 		{
@@ -168,22 +166,22 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			data.grabExcessHorizontalSpace = true;
 			sourceCombo.setLayoutData(data);
 		}
-		
+
 		// add a modify listener that sets the ePackage
 		sourceComboModifyListener = new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				sourceEPackage = sourceIsFileBasedButton.getSelection() ?
 						sourceEcoreEPackages.get(sourceCombo.getText()) :
-						registeredEPackages.get(sourceCombo.getText());	
-				getWizard().getContainer().updateButtons();
+							(EPackage) registeredEPackages.get(sourceCombo.getText());	
+						getWizard().getContainer().updateButtons();
 			}
 		};
-		
+
 		Group targetGroup = new Group(composite, SWT.NONE);
 		{
 			targetGroup.setText("Target Sections ePackage");
-			
+
 			GridLayout layout = new GridLayout();
 			layout.numColumns = 1;
 			layout.marginWidth = marginWidth;
@@ -192,7 +190,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 
 			targetGroup.setLayoutData(new GridData());
 		}
-		
+
 		final Composite targetComposite = new Composite(targetGroup, SWT.NONE);
 		{
 			GridLayout layout = new GridLayout();
@@ -202,29 +200,29 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 
 			targetComposite.setLayoutData(new GridData());
 		}
-		
+
 		// a checkbox to specify if the target ePackage shall be specified by an ecore model
 		targetIsFileBasedButton = new Button(targetComposite, SWT.CHECK);
 		{
-			
+
 			GridData data = new GridData();
 			data.horizontalAlignment = GridData.FILL;
 			data.horizontalSpan = numColumns;
 			targetIsFileBasedButton.setLayoutData(data);
 		}
-		
+
 		targetIsFileBasedButton.setText("Target ePackage is ecore-based");
 		targetIsFileBasedButtonSelectionListener = new SelectionListener() {
-			
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				setTargetIsFileBased(targetIsFileBasedButton.getSelection());
 			}
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {}
 		};
-		
+
 		// This is necessary to allow indent for the file field editor
 		targetFileComposite = new Composite(targetComposite, SWT.NONE);
 		{			
@@ -239,19 +237,19 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			data.horizontalAlignment = GridData.FILL;
 			targetFileComposite.setLayoutData(data);
 		}
-			
+
 		// a file field editor to specify the target ecore model
 		targetFileFieldEditor = new FileFieldEditor("tgtEcoreSelect", "", targetFileComposite);
 		targetFileFieldEditor.setLabelText("Ecore Model:");
 		targetFileFieldEditor.setFileExtensions(new String[]{"*.ecore"});
 		targetFileFieldEditor.setEnabled(false, targetFileComposite);
 		targetFileFieldEditor.setPropertyChangeListener(new IPropertyChangeListener() {
-			
+
 			@Override
 			public void propertyChange(PropertyChangeEvent event) {
-				
+
 				HashMap<String, EPackage> packages = EPackageHelper.getEPackages(targetFileFieldEditor.getStringValue(), true, true);
-				targetEcoreEPackages = (packages == null ? new HashMap<String, EPackage>() : packages);
+				targetEcoreEPackages = (packages == null ? new HashMap<>() : packages);
 				if(targetEcoreEPackages == null) {
 					setErrorMessage("Error while analyzing ePackages in the target ecore model.");
 					targetCombo.removeAll();
@@ -271,15 +269,15 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			data.grabExcessHorizontalSpace = true;
 			targetCombo.setLayoutData(data);
 		}
-		
+
 		// create a modify listener that sets the ePackage
 		targetComboModifyListener = new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				targetEPackage = targetIsFileBasedButton.getSelection() ?
 						targetEcoreEPackages.get(targetCombo.getText()) :
-						registeredEPackages.get(targetCombo.getText());
-				getWizard().getContainer().updateButtons();
+							(EPackage) registeredEPackages.get(targetCombo.getText());
+						getWizard().getContainer().updateButtons();
 			}
 		};
 
@@ -287,19 +285,19 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 		// to both combo boxes
 		updateSourceCombo(registeredEPackages.keySet());
 		updateTargetCombo(registeredEPackages.keySet());
-		
+
 		// realize auto-completion for both combo viewers
 		new AutoCompleteField(sourceCombo, new ComboContentAdapter(), sourceCombo.getItems());
 		new AutoCompleteField(targetCombo, new ComboContentAdapter(), targetCombo.getItems());
-		
+
 		setPageComplete(true);
 		setControl(composite);
 	}
-	
+
 	@Override
 	public void setVisible(boolean visible) {
 		super.setVisible(visible);
-		
+
 		if(visible) {
 			sourceCombo.addModifyListener(sourceComboModifyListener);
 			targetCombo.addModifyListener(targetComboModifyListener);
@@ -312,7 +310,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			targetIsFileBasedButton.removeSelectionListener(targetIsFileBasedButtonSelectionListener);
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @return the ePackage that shall be used for the target sections
@@ -320,7 +318,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 	public EPackage getSourceEPackage() {
 		return sourceEPackage;
 	}
-	
+
 	/**
 	 * 
 	 * @return the path to the ecore model that defines the source ePackage
@@ -328,9 +326,9 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 	public String getSourceEcorePath() {
 		return sourceFileFieldEditor.getStringValue();
 	}
-	
+
 	private boolean setSourceEPackage(EPackage sourceEPackage) {
-		
+
 		for(int i=0; i<this.sourceCombo.getItemCount(); i++) {
 			if(this.sourceCombo.getItem(i).equals(sourceEPackage.getNsURI())) {
 				this.sourceEPackage = sourceEPackage;
@@ -340,7 +338,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Try to set the ePackage in the source combo based on a namespace URI. If the URI is not found
 	 * in the global package registry, the user will have to specify an ecore model that defines the
@@ -359,7 +357,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @return the ePackage that shall be used for the target sections
@@ -367,7 +365,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 	public EPackage getTargetEPackage() {
 		return targetEPackage;
 	}
-	
+
 	/**
 	 * 
 	 * @return the path to the ecore model that defines the target ePackage
@@ -375,7 +373,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 	public String getTargetEcorePath() {
 		return targetFileFieldEditor.getStringValue();
 	}
-	
+
 	private void updateSourceCombo(Set<String> nsUris) {
 		sourceCombo.removeAll();
 		for (String nsUri : nsUris) {
@@ -385,7 +383,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			sourceCombo.select(0);
 		}
 	}
-	
+
 	private void updateTargetCombo(Set<String> nsUris) {
 		targetCombo.removeAll();
 		for (String nsUri : nsUris) {
@@ -395,7 +393,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 			targetCombo.select(0);
 		}
 	}
-	
+
 	private void setSourceIsFileBased(boolean isFileBased) {
 		// if necessary activate/deactivate the checkbox
 		if(sourceIsFileBasedButton.getSelection() != isFileBased) {
@@ -407,11 +405,11 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 		sourceFileFieldEditor.setEnabled(isFileBased, sourceFileComposite);
 		// update the list of ePackages depending on the selection
 		updateSourceCombo(isFileBased ?
-						sourceEcoreEPackages.keySet():
-						registeredEPackages.keySet()
+				sourceEcoreEPackages.keySet():
+					registeredEPackages.keySet()
 				);
 	}
-	
+
 	/**
 	 * 
 	 * @return true if the specified source ePackage comes from an ecore model file, false otherwise
@@ -419,7 +417,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 	public boolean isSourceFileBased() {
 		return sourceIsFileBasedButton.getSelection();
 	}
-	
+
 	/**
 	 * 
 	 * @return true if the specified target ePackage comes from an ecore model file, false otherwise
@@ -427,7 +425,7 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 	public boolean isTargetFileBased() {
 		return targetIsFileBasedButton.getSelection();
 	}
-	
+
 	private void setTargetIsFileBased(boolean isFileBased) {
 		// if necessary activate/deactivate the checkbox
 		if(targetIsFileBasedButton.getSelection() != isFileBased) {
@@ -439,11 +437,11 @@ public class PamtramEPackageSpecificationPage extends WizardPage {
 		targetFileFieldEditor.setEnabled(isFileBased, targetFileComposite);
 		// update the list of ePackages depending on the selection
 		updateTargetCombo(isFileBased ?
-						targetEcoreEPackages.keySet():
-						registeredEPackages.keySet()
+				targetEcoreEPackages.keySet():
+					registeredEPackages.keySet()
 				);
 	}
-	
+
 	@Override
 	public boolean isPageComplete() {
 		return (sourceEPackage != null && targetEPackage != null);

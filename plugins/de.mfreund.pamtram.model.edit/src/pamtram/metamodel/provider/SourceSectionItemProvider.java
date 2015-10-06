bb7ce3dcd83a84344446d0330f33beeb27bbd18e
mfreund <matth.freund@gmail.com>
2015-10-06 18:26:39 +0200

    Implement 'getChoiceOfValue for 'extend' in Source/TargetSectionItemPr.

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/metamodel/provider/SourceSectionItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/metamodel/provider/SourceSectionItemProvider.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
@@ -59,40 +60,63 @@ public class SourceSectionItemProvider extends SourceSectionClassItemProvider {
 	 */
 	protected void addAbstractPropertyDescriptor(Object object) {
 		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
+		(createItemPropertyDescriptor
 				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_Section_abstract_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_Section_abstract_feature", "_UI_Section_type"),
-				 MetamodelPackage.Literals.SECTION__ABSTRACT,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
-				 null,
-				 null));
+						getResourceLocator(),
+						getString("_UI_Section_abstract_feature"),
+						getString("_UI_PropertyDescriptor_description", "_UI_Section_abstract_feature", "_UI_Section_type"),
+						MetamodelPackage.Literals.SECTION__ABSTRACT,
+						true,
+						false,
+						false,
+						ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
+						null,
+						null));
 	}
 
 	/**
 	 * This adds a property descriptor for the Extend feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	protected void addExtendPropertyDescriptor(Object object) {
 		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
+		(new ItemPropertyDescriptor
 				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_Section_extend_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_Section_extend_feature", "_UI_Section_type"),
-				 MetamodelPackage.Literals.SECTION__EXTEND,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+						getResourceLocator(),
+						getString("_UI_Section_extend_feature"),
+						getString("_UI_PropertyDescriptor_description", "_UI_Section_extend_feature", "_UI_Section_type"),
+						MetamodelPackage.Literals.SECTION__EXTEND,
+						true,
+						false,
+						true,
+						null,
+						null,
+						null) {
+
+			@Override
+			public Collection<?> getChoiceOfValues(Object object) {
+
+				SourceSection sourceSection = (SourceSection) object;
+
+				if(sourceSection.getEClass() == null) {
+					return new BasicEList<>();
+				}
+
+				Collection<SourceSection> values = (Collection<SourceSection>) super.getChoiceOfValues(object);
+				Collection<SourceSection> ret = new BasicEList<>();
+
+				for (SourceSection val : values) {
+					// only abstract sections that have a matching eClass can be used as extended section
+					if(val.isAbstract() && (val.eClass() == sourceSection.eClass() || val.getEClass().getESuperTypes().contains(sourceSection.getEClass()))) {
+						ret.add(val);
+					}
+				}
+
+				return ret;
+			}
+		});
 	}
 
 	/**
@@ -103,18 +127,18 @@ public class SourceSectionItemProvider extends SourceSectionClassItemProvider {
 	 */
 	protected void addReferencingMappingsPropertyDescriptor(Object object) {
 		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
+		(createItemPropertyDescriptor
 				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_SourceSection_referencingMappings_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_SourceSection_referencingMappings_feature", "_UI_SourceSection_type"),
-				 MetamodelPackage.Literals.SOURCE_SECTION__REFERENCING_MAPPINGS,
-				 false,
-				 false,
-				 false,
-				 null,
-				 null,
-				 null));
+						getResourceLocator(),
+						getString("_UI_SourceSection_referencingMappings_feature"),
+						getString("_UI_PropertyDescriptor_description", "_UI_SourceSection_referencingMappings_feature", "_UI_SourceSection_type"),
+						MetamodelPackage.Literals.SOURCE_SECTION__REFERENCING_MAPPINGS,
+						false,
+						false,
+						false,
+						null,
+						null,
+						null));
 	}
 
 	/**
@@ -152,9 +176,9 @@ public class SourceSectionItemProvider extends SourceSectionClassItemProvider {
 		updateChildren(notification);
 
 		switch (notification.getFeatureID(SourceSection.class)) {
-			case MetamodelPackage.SOURCE_SECTION__ABSTRACT:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-				return;
+		case MetamodelPackage.SOURCE_SECTION__ABSTRACT:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+			return;
 		}
 		super.notifyChanged(notification);
 	}

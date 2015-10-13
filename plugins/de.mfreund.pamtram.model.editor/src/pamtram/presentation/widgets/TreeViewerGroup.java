1f673061554d1c94c97f43bca20fcda93d421689
mfreund <matth.freund@gmail.com>
2015-10-13 15:32:00 +0200

    Use the StyledLabelProvider in the TreeViewerGroup

--- plugins/de.mfreund.pamtram.model.editor/src/pamtram/presentation/widgets/TreeViewerGroup.java
+++ plugins/de.mfreund.pamtram.model.editor/src/pamtram/presentation/widgets/TreeViewerGroup.java
@@ -14,6 +14,7 @@ import org.eclipse.emf.edit.ui.action.CreateSiblingAction;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.emf.edit.ui.provider.DecoratingColumLabelProvider;
+import org.eclipse.emf.edit.ui.provider.DelegatingStyledCellLabelProvider;
 import org.eclipse.emf.edit.ui.provider.DiagnosticDecorator;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.MenuManager;
@@ -40,8 +41,8 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.FilteredTree;
 import org.eclipse.ui.dialogs.PatternFilter;
 
-import pamtram.presentation.PamtramEditorPlugin;
 import de.mfreund.pamtram.util.BundleContentHelper;
+import pamtram.presentation.PamtramEditorPlugin;
 
 /**
  * A class that represents an SWT group containing a filtered tree viewer and 
@@ -51,20 +52,20 @@ import de.mfreund.pamtram.util.BundleContentHelper;
  *
  */
 public class TreeViewerGroup extends FilteredTree{
-	
+
 	private final String bundleID = PamtramEditorPlugin.getPlugin().getSymbolicName();
-	
+
 	/**
 	 * This is the adapter factory used to create content and
 	 * label providers.
 	 */
 	private ComposedAdapterFactory adapterFactory;
-	
+
 	/**
 	 * This is the group that contains all other parts.
 	 */
 	private Group group;
-	
+
 	/**
 	 * This is the tool bar.
 	 */
@@ -80,7 +81,7 @@ public class TreeViewerGroup extends FilteredTree{
 	 * (besides standard icons like 'collapseAll').
 	 */
 	private ArrayList<Image> toolbarImages;
-	
+
 	/**
 	 * A setter for the tool bar images that checks for 'null' parameter.
 	 * 
@@ -101,14 +102,14 @@ public class TreeViewerGroup extends FilteredTree{
 	 * Whether to display a 'collapseAll' button in the tool bar.
 	 */
 	private boolean displayCollapseAll;
-	
+
 	/**
 	 * Whether to display an 'add' button in the tool bar.
 	 */
 	private boolean displayAdd;
 
 	private EditingDomain editingDomain;
-	
+
 	/**
 	 * A setter for the tool bar listeners that checks for 'null' parameter.
 	 * 
@@ -118,7 +119,7 @@ public class TreeViewerGroup extends FilteredTree{
 		this.toolbarListeners = 
 				(toolbarListeners != null ? toolbarListeners : new ArrayList<SelectionListener>()); 
 	}
-	
+
 	/**
 	 * Use this constructor if you do not want to add a tool bar to the viewer.
 	 * 
@@ -142,7 +143,7 @@ public class TreeViewerGroup extends FilteredTree{
 		this.adapterFactory = adapterFactory;
 		this.init(SWT.MULTI, new PatternFilter());
 	}
-	
+
 	/**
 	 * Use this constructor if you want to add a tool bar to the viewer.
 	 * 
@@ -171,7 +172,7 @@ public class TreeViewerGroup extends FilteredTree{
 		this.editingDomain = editingDomain;
 		this.init(SWT.MULTI, new PatternFilter());
 	}
-	
+
 	/**
 	 * Create the filtered tree's controls. This is copied from the 
 	 * standard 'FilteredTree' - changes are only introduced in the
@@ -182,7 +183,7 @@ public class TreeViewerGroup extends FilteredTree{
 	 */
 	@Override
 	protected void createControl(Composite parent, int treeStyle) {
-		
+
 		// Set the layout for the main composite
 		if (parent.getLayout() instanceof GridLayout) {
 			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
@@ -198,9 +199,9 @@ public class TreeViewerGroup extends FilteredTree{
 		group.setLayoutData(
 				new GridData(SWT.FILL, SWT.FILL, true, true));
 		group.setLayout(new GridLayout(1, true));
-		
+
 		if (showFilterControls) {
-			
+
 			// This composite hosts two children: the filter composite and the
 			// tool bar composite.
 			filterComposite= new Composite(group, SWT.NONE);
@@ -224,7 +225,7 @@ public class TreeViewerGroup extends FilteredTree{
 			filterBoxComposite.setLayout(filterLayout);
 			filterBoxComposite.setFont(parent.getFont());
 			createFilterControls(filterBoxComposite);
-			
+
 			// create a toolbar besides the filter controls
 			Composite toolbarComposite = new Composite(filterComposite, SWT.NONE);
 			toolbarComposite.setLayoutData(
@@ -235,7 +236,7 @@ public class TreeViewerGroup extends FilteredTree{
 			toolbarComposite.setLayout(toolbarLayout);
 			createToolbar(toolbarComposite, toolbarImages, toolbarListeners, 
 					displayCollapseAll, displayAdd);
-			
+
 		}
 
 		treeComposite = new Composite(group, SWT.NONE);
@@ -246,9 +247,9 @@ public class TreeViewerGroup extends FilteredTree{
 		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 		treeComposite.setLayoutData(data);
 		createTreeControl(treeComposite, treeStyle);
-		
+
 	}
-	
+
 	/**
 	 * Override the standard method so that default label and content providers
 	 * based on the adapter factory are created.
@@ -260,13 +261,13 @@ public class TreeViewerGroup extends FilteredTree{
 	@Override
 	protected TreeViewer doCreateTreeViewer(Composite parent, int style) {
 		TreeViewer treeViewer = super.doCreateTreeViewer(parent, style);
-		treeViewer.setLabelProvider(new DecoratingColumLabelProvider(
-				new AdapterFactoryLabelProvider(adapterFactory), 
-				new DiagnosticDecorator(editingDomain, treeViewer, PamtramEditorPlugin.getPlugin().getDialogSettings())));
+		treeViewer.setLabelProvider(new DelegatingStyledCellLabelProvider(new DecoratingColumLabelProvider.StyledLabelProvider(
+				new AdapterFactoryLabelProvider.StyledLabelProvider(adapterFactory, treeViewer), 
+				new DiagnosticDecorator.Styled(editingDomain, treeViewer, PamtramEditorPlugin.getPlugin().getDialogSettings()))));
 		treeViewer.setContentProvider(new AdapterFactoryContentProvider(adapterFactory));
 		new ColumnViewerInformationControlToolTipSupport(
 				treeViewer, 
-				new DiagnosticDecorator.EditingDomainLocationListener(editingDomain, treeViewer));
+				new DiagnosticDecorator.Styled.EditingDomainLocationListener(editingDomain, treeViewer));
 		return treeViewer;
 	}
 
@@ -282,60 +283,60 @@ public class TreeViewerGroup extends FilteredTree{
 	private void createToolbar(Composite parent, ArrayList<Image> images,
 			ArrayList<SelectionListener> listeners, boolean displayCollapseAll,
 			boolean displayAdd) {
-		
+
 		if(images.size() != listeners.size()) {
 			throw new RuntimeException("The number of images provided must match"
 					+ " the number of listeners provided!");
 		}
-		
+
 		if(images.isEmpty() && listeners.isEmpty() && !displayCollapseAll && !displayAdd) {
 			// nothing to be done
 			return;
 		}
-		
+
 		// Create the button area
 		toolbar = new ToolBar(parent, SWT.NONE);
 		toolbar.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-	    
-	    // Add the 'collapseAll' tool bar item
-	    if(displayCollapseAll) {
-	    	ToolItem item = new ToolItem(toolbar, SWT.PUSH | SWT.TRAIL);
-	    	item.setImage(BundleContentHelper.getBundleImage(bundleID, "icons/collapse_all.gif"));
+
+		// Add the 'collapseAll' tool bar item
+		if(displayCollapseAll) {
+			ToolItem item = new ToolItem(toolbar, SWT.PUSH | SWT.TRAIL);
+			item.setImage(BundleContentHelper.getBundleImage(bundleID, "icons/collapse_all.gif"));
 			item.addSelectionListener(new SelectionListener() {
-				
+
 				@Override
 				public void widgetSelected(SelectionEvent e) {
 					getViewer().collapseAll();
 				}
-				
+
 				@Override
 				public void widgetDefaultSelected(SelectionEvent e) {}
 			});
-	    }
-	    
-	    // Add the 'add' tool bar item
-	    if(displayAdd) {
-	    	ToolItem item = new ToolItem(toolbar, SWT.DROP_DOWN);
-	    	item.setImage(BundleContentHelper.getBundleImage(bundleID, "icons/add_obj.gif"));
-	    	
-	        item.addSelectionListener(
-	        		new AddDropDownSelectionListener(item));
-	    }
-		
-	    // Add the various tool bar items passed in the constructor
+		}
+
+		// Add the 'add' tool bar item
+		if(displayAdd) {
+			ToolItem item = new ToolItem(toolbar, SWT.DROP_DOWN);
+			item.setImage(BundleContentHelper.getBundleImage(bundleID, "icons/add_obj.gif"));
+
+			item.addSelectionListener(
+					new AddDropDownSelectionListener(item));
+		}
+
+		// Add the various tool bar items passed in the constructor
 		for (int i=0; i<images.size(); i++) {
 			ToolItem item = new ToolItem(toolbar, SWT.PUSH | SWT.TRAIL);
 			item.setImage(images.get(i));
 			item.addSelectionListener(listeners.get(i));
 		}
-		
+
 		// Add additional stuff to the tool bar
 		createAdditionalToolbarItems(toolbar);
-		
+
 		toolbar.pack();
 
 	}
-	
+
 	/**
 	 * This creates additional tool bar items besides the standard items
 	 * like 'collapseAll'. The default implementation does nothing - 
@@ -344,33 +345,33 @@ public class TreeViewerGroup extends FilteredTree{
 	 * @param toolbar The parent tool bar.
 	 */
 	protected void createAdditionalToolbarItems(ToolBar toolbar) {
-		
+
 		// allow extending classes to easily configure the tool bar
 	}
-	
+
 	class AddDropDownSelectionListener extends SelectionAdapter {
-		
+
 		private MenuManager menuManager;
 
 		public AddDropDownSelectionListener(ToolItem dropdown) {
 			menuManager = new MenuManager();
 			menuManager.createContextMenu(dropdown.getParent());
 		}
-		
+
 		@Override
 		public void widgetSelected(SelectionEvent event) {
 			if (event.detail == SWT.ARROW) {
-		    	ToolItem item = (ToolItem) event.widget;
-		    	Rectangle rect = item.getBounds();
-		    	Point pt = item.getParent().toDisplay(new Point(rect.x, rect.y));
-		    	createMenu();
-		    	menuManager.getMenu().setLocation(pt.x, pt.y + rect.height);
-		    	menuManager.getMenu().setVisible(true);
-		    } else {
-		    	addDefaultElement();
-		    }
+				ToolItem item = (ToolItem) event.widget;
+				Rectangle rect = item.getBounds();
+				Point pt = item.getParent().toDisplay(new Point(rect.x, rect.y));
+				createMenu();
+				menuManager.getMenu().setLocation(pt.x, pt.y + rect.height);
+				menuManager.getMenu().setVisible(true);
+			} else {
+				addDefaultElement();
+			}
 		}
-		
+
 		/**
 		 * This determines the currently selected element and adds
 		 * an element of the same type to its parent.
@@ -387,7 +388,7 @@ public class TreeViewerGroup extends FilteredTree{
 				// nothing to be done
 				return;
 			}
-			
+
 			// A command that will create a new eObject that is of the same
 			// type of the selected eObject. The newly created object will then
 			// be added to the parent.
@@ -398,23 +399,23 @@ public class TreeViewerGroup extends FilteredTree{
 					EcoreUtil.create(
 							((EObject) selectedObject).eClass()),
 					null);
-			
+
 			// Execute the command and set the selection of the viewer to the
 			// newly created object.
 			editingDomain.getCommandStack().execute(command);
 			treeViewer.setSelection(new StructuredSelection(command.getResult().toArray()));
 		}
-		
+
 		/**
 		 * This creates a new menu based on the currently selected 
 		 * element that allows to create children and siblings.
 		 */
 		private void createMenu() {
-			
+
 			// Clear the context menu
 			//
 			menuManager.removeAll();
-			
+
 			// Query the new selection for appropriate new child/sibling descriptors
 			//
 			Collection<?> newChildDescriptors = null;
@@ -422,12 +423,12 @@ public class TreeViewerGroup extends FilteredTree{
 			ISelection selection = treeViewer.getSelection();
 			if (!(selection instanceof IStructuredSelection) || 
 					!(((IStructuredSelection)selection).size() == 1)) {
-						// nothing to be done
-						// TODO if nothing is selected, the user should be allowed
-						// to add top-level elements
-						return;
+				// nothing to be done
+				// TODO if nothing is selected, the user should be allowed
+				// to add top-level elements
+				return;
 			}
-			
+
 			Object object = ((IStructuredSelection)selection).getFirstElement();
 
 			newChildDescriptors = editingDomain.getNewChildDescriptors(object, null);
@@ -435,7 +436,7 @@ public class TreeViewerGroup extends FilteredTree{
 
 			IEditorPart editorPart = 
 					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-			
+
 			// Generate actions for selection; populate and redraw the menus.
 			//
 			ArrayList<IAction> createChildActions = new ArrayList<>();

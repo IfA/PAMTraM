89136304b260cbf749d354b6238fbfe955dc60df
mfreund <matth.freund@gmail.com>
2015-10-26 18:01:21 +0100

    Create class 'DefaultAmbiguityResolvingStrategy'
    This will be used if no other strategy is provided. It simply returns
    the first element in the list of choices.

---
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/DefaultAmbiguityResolvingStrategy.java
@@ -0,0 +1,75 @@
+package de.mfreund.gentrans.transformation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+
+import pamtram.mapping.Mapping;
+import pamtram.mapping.MappingInstanceSelector;
+import pamtram.mapping.ModelConnectionHint;
+import pamtram.metamodel.TargetSectionNonContainmentReference;
+
+/**
+ * This represents a default implementation of the {@link IAmbiguityResolvingStrategy} interface.
+ * <p />
+ * Any call to a method defined by the {@link IAmbiguityResolvingStrategy} interface will simply return the first element
+ * in the list of choices.
+ * 
+ * @author mfreund
+ */
+public class DefaultAmbiguityResolvingStrategy implements IAmbiguityResolvingStrategy {
+
+	@Override
+	public List<Mapping> resolveMatchingAmbiguity(List<Mapping> choices, EObject element) {
+
+		if(choices == null) {
+			return null;
+		} else if(choices.size() == 0) {
+			return new ArrayList<>();
+		} else {
+			return new ArrayList<>(Arrays.asList(choices.get(0)));
+		}
+	}
+
+	@Override
+	public List<EObjectWrapper> resolveJoiningAmbiguity(List<EObjectWrapper> choices, EObjectWrapper element,
+			ModelConnectionHint modelConnectionHint, String hintValue) {
+
+		if(choices == null) {
+			return null;
+		} else if(choices.size() == 0) {
+			return new ArrayList<>();
+		} else {
+			return new ArrayList<>(Arrays.asList(choices.get(0)));
+		}
+	}
+
+	@Override
+	public List<EObjectWrapper> resolveLinkingAmbiguity(List<EObjectWrapper> choices,
+			TargetSectionNonContainmentReference reference, MappingInstanceSelector mappingInstanceSelector,
+			EObjectWrapper sourceElement) {
+
+		if(choices == null) {
+			return null;
+		} else if(choices.size() == 0) {
+			return new ArrayList<>();
+		} else {
+			return new ArrayList<>(Arrays.asList(choices.get(0)));
+		}
+	}
+
+	@Override
+	public List<EClass> resolveRootElementAmbiguity(List<EClass> choices) {
+
+		if(choices == null) {
+			return null;
+		} else if(choices.size() == 0) {
+			return new ArrayList<>();
+		} else {
+			return new ArrayList<>(Arrays.asList(choices.get(0)));
+		}
+	}
+}

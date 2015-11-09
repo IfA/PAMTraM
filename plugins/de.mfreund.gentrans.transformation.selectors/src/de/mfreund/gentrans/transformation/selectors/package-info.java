3f44ce326c812a411f84d6ccbd176ceb2776c5ab
mfreund <matth.freund@gmail.com>
2015-11-09 17:41:18 +0100

    Add 'package-info' for the sub-package 'selectors'

---
+++ plugins/de.mfreund.gentrans.transformation.selectors/src/de/mfreund/gentrans/transformation/selectors/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes that implement the various wizards used by the 
+ * {@link de.mfreund.gentrans.transformation.UserDecisionResolvingStrategy UserDecisionResolvingStrategy}.
+ *
+ * @author mfreund
+ */
+package de.mfreund.gentrans.transformation.selectors;

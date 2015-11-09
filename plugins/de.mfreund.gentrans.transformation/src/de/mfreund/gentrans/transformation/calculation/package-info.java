5247b70b0143065a554b2c4044db97f97a9469fe
mfreund <matth.freund@gmail.com>
2015-11-09 17:33:24 +0100

    Create 'package-iinfo' for sub-package 'calculation'

---
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/calculation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes that define new {@link de.congrace.exp4j.CustomFunction CustomFunctions} that
+ * can be used to calculate attribute values during the transformation.
+ *  
+ * @author mfreund
+ */
+package de.mfreund.gentrans.transformation.calculation;

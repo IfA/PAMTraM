d26e302edff3f272c315df578546fc9885c60d61
mfreund <matth.freund@gmail.com>
2015-11-16 11:03:46 +0100

    Add constructor that stores the path to the transformation to be used

--- plugins/de.mfreund.gentrans.transformation.resolving.history/src/de/mfreund/gentrans/transformation/resolving/history/HistoryResolvingStrategy.java
+++ plugins/de.mfreund.gentrans.transformation.resolving.history/src/de/mfreund/gentrans/transformation/resolving/history/HistoryResolvingStrategy.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import de.mfreund.gentrans.transformation.resolving.ComposedAmbiguityResolvingStrategy;
 import de.mfreund.gentrans.transformation.resolving.IAmbiguityResolvingStrategy;
+import de.mfreund.pamtram.transformation.Transformation;
 
 /**
  * This class implements a concrete {@link ComposedAmbiguityResolvingStrategy} that consults previous resolving 
@@ -18,8 +19,26 @@ import de.mfreund.gentrans.transformation.resolving.IAmbiguityResolvingStrategy;
  */
 public class HistoryResolvingStrategy extends ComposedAmbiguityResolvingStrategy {
 
-	public HistoryResolvingStrategy(ArrayList<IAmbiguityResolvingStrategy> composedStrategies) {
+	/**
+	 * The path to the {@link Transformation TransformationModel} to be used to 
+	 * consult previous resolving results
+	 */
+	private String transformationModelPath;
+
+	private HistoryResolvingStrategy(ArrayList<IAmbiguityResolvingStrategy> composedStrategies) {
 		super(composedStrategies);
+	}
+
+	/**
+	 * This creates an instance.
+	 * 
+	 * @param composedStrategies The list of {@link IAmbiguityResolvingStrategy strategies} that this composes.
+	 * @param transformationModelPath The path to the {@link Transformation TransformationModel} to be used to 
+	 * consult previous resolving results.
+	 */
+	public  HistoryResolvingStrategy(ArrayList<IAmbiguityResolvingStrategy> composedStrategies, String transformationModelPath) {
+		this(composedStrategies);
+		this.transformationModelPath = transformationModelPath;
 	}
 
 }

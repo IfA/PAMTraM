bbf1a28bfe44dd52403ec2144beb20cf50f6032a
mfreund <matth.freund@gmail.com>
2015-11-16 18:10:04 +0100

    Create method 'initializeEMFCompare'
    This initializes the comparison scope and everything else and currently
    compares the old and the new pamtram model.

--- plugins/de.mfreund.gentrans.transformation.resolving.history/src/de/mfreund/gentrans/transformation/resolving/history/HistoryResolvingStrategy.java
+++ plugins/de.mfreund.gentrans.transformation.resolving.history/src/de/mfreund/gentrans/transformation/resolving/history/HistoryResolvingStrategy.java
@@ -3,6 +3,19 @@ package de.mfreund.gentrans.transformation.resolving.history;
 import java.util.ArrayList;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.EMFCompare;
+import org.eclipse.emf.compare.match.DefaultComparisonFactory;
+import org.eclipse.emf.compare.match.DefaultEqualityHelperFactory;
+import org.eclipse.emf.compare.match.DefaultMatchEngine;
+import org.eclipse.emf.compare.match.IComparisonFactory;
+import org.eclipse.emf.compare.match.IMatchEngine;
+import org.eclipse.emf.compare.match.eobject.IEObjectMatcher;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryImpl;
+import org.eclipse.emf.compare.match.impl.MatchEngineFactoryRegistryImpl;
+import org.eclipse.emf.compare.scope.DefaultComparisonScope;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.compare.utils.UseIdentifiers;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -69,6 +82,7 @@ public class HistoryResolvingStrategy extends ComposedAmbiguityResolvingStrategy
 		this.pamtramModel = pamtramModel;
 		this.sourceModels = sourceModels;
 		loadTransformationModel();
+		initializeEMFCompare();
 	}
 
 	/**
@@ -88,6 +102,32 @@ public class HistoryResolvingStrategy extends ComposedAmbiguityResolvingStrategy
 			throw new RuntimeException("The transformation model does not contain a stored transformation.");
 		}
 		transformationModel = (Transformation) transformationModelResource.getContents().get(0);
+	}
+
+	/**
+	 * Initialize the comparison scope, configure the comparison and compare both the PAMTraM models (the one
+	 * stored as part of the {@link #transformationModel} and the {@link #pamtramModel}) as well as the
+	 * source models (those stored as part of the {@link #transformationModel} and the {@link #sourceModels}).
+	 * <p/>
+	 * Note: This is taken from <a href="https://www.eclipse.org/emf/compare/documentation/latest/developer/developer-guide.html#Using_The_Compare_APIs">
+	 * https://www.eclipse.org/emf/compare/documentation/latest/developer/developer-guide.html#Using_The_Compare_APIs</a>.
+	 */
+	private void initializeEMFCompare() {
+
+		IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
+		IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory());
+
+		IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory);
+		matchEngineFactory.setRanking(20);
+		IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl();
+		matchEngineRegistry.add(matchEngineFactory);
+
+		IComparisonScope scope = new DefaultComparisonScope(this.pamtramModel, this.transformationModel.getPamtramInstance(), null);
+		EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build();
+
+		Comparison compareResult = comparator.compare(scope);
+
+		System.out.println();
 	}
 
 }

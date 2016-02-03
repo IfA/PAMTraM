0cd45c60adad8afb2bfe6a48fde14e83470393bd
mfreund <matth.freund@gmail.com>
2016-02-03 17:04:17 +0100

    Create class 'SetViewerMouseListener'
    This handles the setting of the 'currentViewer' in the Pamtram editor.

---
+++ plugins/de.mfreund.pamtram.model.editor/src/pamtram/presentation/SetViewerMouseListener.java
@@ -0,0 +1,54 @@
+package pamtram.presentation;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * A {@link MouseListener} that triggers {@link PamtramEditor#setCurrentViewer(Viewer)}.
+ * <p />
+ * An instance of this should be attached to all {@link Control Controls} that display an excerpt
+ * of the PAMTraM model. It ensures that the correct 'current viewer' is set before other listeners
+ * (e.g. a SelectionListener) are triggered and thus that no selection events are mysteriously lost.
+ * 
+ * @author mfreund
+ */
+final public class SetViewerMouseListener implements MouseListener {
+	
+	/**
+	 * The {@link PamtramEditor} for that the viewer shall be {@link PamtramEditor#setCurrentViewer(Viewer) activated}
+	 * if this listener is triggered. 
+	 */
+	private PamtramEditor editor;
+	
+	/**
+	 * The viewer that shall be {@link PamtramEditor#setCurrentViewer(Viewer) activated} upon
+	 * if this listener is triggered.
+	 */
+	private Viewer viewer;
+	
+	/**
+	 * This creates an instance.
+	 * 
+	 * @param editor The {@link PamtramEditor} for that the viewer shall be {@link PamtramEditor#setCurrentViewer(Viewer) activated}
+	 * if this listener is triggered. 
+	 * @param viewer The viewer that shall be {@link PamtramEditor#setCurrentViewer(Viewer) activated} upon
+	 * if this listener is triggered.
+	 */
+	public SetViewerMouseListener(PamtramEditor editor, Viewer viewer) {
+		this.editor = editor;
+		this.viewer = viewer;
+	}
+	
+	@Override
+	public void mouseUp(MouseEvent e) {}
+
+	@Override
+	public void mouseDown(MouseEvent e) {
+		editor.setCurrentViewer(viewer);
+	}
+
+	@Override
+	public void mouseDoubleClick(MouseEvent e) {}
+}

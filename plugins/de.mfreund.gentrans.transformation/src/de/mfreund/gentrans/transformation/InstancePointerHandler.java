6ba8e5c120d98051b95006a8046f730df25ec00c
Gennadiy Koltun <gennadiy_dmitrovic.koltun@tu-dresden.de>
2016-03-25 16:43:38 +0100

    InstancePointerHandler ready for checking

--- plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/InstancePointerHandler.java
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/InstancePointerHandler.java
@@ -1,11 +1,18 @@
 package de.mfreund.gentrans.transformation;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ui.console.MessageConsoleStream;
 
+import pamtram.metamodel.InstancePointer;
+import pamtram.metamodel.SourceSectionAttribute;
 import pamtram.metamodel.SourceSectionClass;
 
 /**
@@ -21,14 +28,59 @@ import pamtram.metamodel.SourceSectionClass;
 	 * where the key is the corresponding {@link SourceSectionClass} that they have been matched to.
 	 */
 	private LinkedHashMap<SourceSectionClass, Set<EObject>> matchedSections;
+	
+	/**
+	 * The {@link MessageConsoleStream} that shall be used to print messages.
+	 */
+	private final MessageConsoleStream consoleStream;
 	 
 	// Constructor
-	public InstancePointerHandler(LinkedHashMap<SourceSectionClass, Set<EObject>> matchedSections){
+	public InstancePointerHandler(LinkedHashMap<SourceSectionClass, Set<EObject>> matchedSections, MessageConsoleStream consoleStream){
 		this.matchedSections = matchedSections;
+		this.consoleStream =consoleStream;
 	}
 	
-	public ArrayList<EObject> getPointedInstance(EObject instancePointerObt, ArrayList<EObject> listObts){
-		return listObts;
+	public EObject getPointedInstanceByMatchedSectionRepo(InstancePointer instancePointerObt, SourceSectionAttribute sourceAttribute){
+		
+		Set<EObject> possiblePointedClasses = matchedSections.get(sourceAttribute.eContainer());
+		// alternatively we get the container-Class by the InstancePointer pointed EAttribute
+		//Set<EObject> possiblePointedClasses = matchedSections.get(instancePointerObt.getAttributePointer().eContainer());
+		
+		for(Iterator<EObject> element = possiblePointedClasses.iterator(); element.hasNext();){
+			EObject eClass = element.next();
+			
+			if(!(eClass.eGet((EStructuralFeature) instancePointerObt.getAttributePointer()).equals(instancePointerObt.getValue()))){
+				element.remove();
+			}
+		}
+		
+		if(possiblePointedClasses.size()==1){
+			return possiblePointedClasses.iterator().next();
+		} else {
+			consoleStream.println("InstancePointer result is not clear! The correspond set is of size:" + possiblePointedClasses.size()
+			+ "\n");
+			return null;
+		}
+	}
+		
+	public EObject getPointedInstanceByList(InstancePointer instancePointerObt, SourceSectionAttribute sourceAttribute, ArrayList<EObject> attrList){
+		
+		for(Iterator<EObject> element = attrList.iterator(); element.hasNext();){
+			EObject eAttr = element.next();
+			EObject eClass = eAttr.eContainer();
+			
+			if(!(eClass.eGet((EStructuralFeature) instancePointerObt.getAttributePointer()).equals(instancePointerObt.getValue()))){
+				element.remove();
+			}
+		}
+		
+		if(attrList.size()==1){
+			return attrList.iterator().next();
+		} else {
+			consoleStream.println("InstancePointer result is not clear! The correspond set is of size:" + attrList.size()
+			+ "\n");
+			return null;
+		}
 	}
 	
 	/**

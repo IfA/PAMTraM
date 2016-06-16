e856a7852a1586e7ac6625a24015d3e30701d5ae
mfreund <matth.freund@gmail.com>
2016-06-16 13:55:28 +0200

    Fix two NPEs

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/LocalModifiedAttributeElementTypeItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/mapping/provider/LocalModifiedAttributeElementTypeItemProvider.java
@@ -20,7 +20,6 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.StyledString;
 import pamtram.mapping.LocalModifiedAttributeElementType;
 import pamtram.mapping.Mapping;
-import pamtram.mapping.MappingHintGroupImporter;
 import pamtram.mapping.MappingHintGroupType;
 import pamtram.mapping.MappingPackage;
 import pamtram.mapping.ModelConnectionHintTargetAttribute;
@@ -140,26 +139,15 @@ public class LocalModifiedAttributeElementTypeItemProvider extends ModifiedAttri
 			@Override
 			public Collection<?> getChoiceOfValues(Object object) {
 
-				//the parent Mapping Hint Group
-				EObject parent=((ModifiedAttributeElementType<?,?,?,?>) object).eContainer();
-				// the parent mapping
-				Mapping mapping;
-				while(true){
-					if(parent instanceof MappingHintGroupType){
-						mapping=(Mapping)((MappingHintGroupType) parent).eContainer();
-						break;
-					} else if(parent instanceof MappingHintGroupImporter){
-						mapping=(Mapping)((MappingHintGroupImporter) parent).eContainer();
-						break;
-					} else if(parent instanceof Mapping) {
-						mapping = (Mapping) parent;
-						break;
-					} else {
-						parent=parent.eContainer();
-					}
-				}
+				//the parent MappingHintGroup
+				//
+				EObject parent=((ModifiedAttributeElementType<?,?,?,?>) object).getMappingHintGroup();
+				
+				// the parent Mapping
+				//
+				Mapping mapping = ((ModifiedAttributeElementType<?,?,?,?>) object).getMapping();
 
-				Class relevantClass = null;
+				Class<?,?,?,?> relevantClass = null;
 
 				// If we operate on an ModelConnectionHintTarget, we need to scan the container of the target section.
 				//
@@ -175,19 +163,19 @@ public class LocalModifiedAttributeElementTypeItemProvider extends ModifiedAttri
 					
 				// Otherwise, we need to scan the source section for suitable attributes.
 				//
-				} else {
+				} else if(mapping != null) {
 					relevantClass = mapping.getSourceMMSection();
 				}
 
 				if(relevantClass == null) {
-					return new ArrayList<Object>();						
+					return new ArrayList<>();						
 				}
 
-				List<Object> choiceOfValues = new ArrayList<Object>();
+				List<Object> choiceOfValues = new ArrayList<>();
 
 				// iterate over all elements and return the attributes as possible options
-				Set<Class> scanned=new HashSet<>();
-				List<Class> sectionsToScan=new ArrayList<>();
+				Set<Class<?,?,?,?>> scanned = new HashSet<>();
+				List<Class<?,?,?,?>> sectionsToScan = new ArrayList<>();
 				sectionsToScan.add(relevantClass);
 				
 				// also regard abstract sections that this extends
@@ -195,8 +183,8 @@ public class LocalModifiedAttributeElementTypeItemProvider extends ModifiedAttri
 					sectionsToScan.addAll(((Section<?, ?, ?, ?>) relevantClass).getExtend()); 
 				}
 
-				while(sectionsToScan.size() > 0){
-					Class classToScan=sectionsToScan.remove(0);
+				while(!sectionsToScan.isEmpty()){
+					Class<?,?,?,?> classToScan=sectionsToScan.remove(0);
 					scanned.add(classToScan);
 
 					Iterator<EObject> it = classToScan.eAllContents();
@@ -205,7 +193,7 @@ public class LocalModifiedAttributeElementTypeItemProvider extends ModifiedAttri
 						if(next instanceof pamtram.metamodel.Attribute) {
 							choiceOfValues.add(next);
 						} else if(next instanceof MetaModelSectionReference){
-							List<SourceSectionClass> vals=new ArrayList<SourceSectionClass>();
+							List<SourceSectionClass> vals = new ArrayList<>();
 							vals.addAll(((MetaModelSectionReference) next).getValue());
 							vals.removeAll(scanned);
 							sectionsToScan.addAll(vals);

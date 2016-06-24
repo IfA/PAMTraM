41cdd2555e2fd5ec437ea9132b12f7ec4424da2d
mfreund <matth.freund@gmail.com>
2016-06-24 09:16:37 +0200

    Some refactoring and documentation for the ExpressionCalculator

--- plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/calculation/ExpressionCalculator.java
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/calculation/ExpressionCalculator.java
@@ -6,8 +6,6 @@ import org.eclipse.ui.console.MessageConsoleStream;
 
 import de.congrace.exp4j.ExpressionBuilder;
 import de.congrace.exp4j.InvalidCustomFunctionException;
-import de.congrace.exp4j.UnknownFunctionException;
-import de.congrace.exp4j.UnparsableExpressionException;
 import de.mfreund.gentrans.transformation.calculation.MaxFunction;
 import de.mfreund.gentrans.transformation.calculation.MinFunction;
 import de.mfreund.gentrans.transformation.calculation.RoundFunction;
@@ -20,30 +18,33 @@ import pamtram.metamodel.TargetSectionAttribute;
 /**
  * This class can be used to handle and calculate general expressions.
  * 
- * @author gkoltun
  */
 public class ExpressionCalculator {
 	
 	/**
-	 * RoundFunction instance, maybe needed when inside of an expression defined
+	 * {@link RoundFunction} instance, maybe needed when inside of an expression defined
 	 */
 	private RoundFunction round;
 	
 	/**
-	 * MaxFunction instance, maybe needed when inside of an expression defined
+	 * {@link MaxFunction} instance, maybe needed when inside of an expression defined
 	 */
 	private MaxFunction max;
 	
 	/**
-	 * MinFunction instance, maybe needed when inside of an expression defined
+	 * {@link MinFunction} instance, maybe needed when inside of an expression defined
 	 */
 	private MinFunction min;
 	
 	/**
-	 * The console stream to be used to print messages.
+	 * The {@link MessageConsoleStream} to be used to print messages.
 	 */
 	private MessageConsoleStream consoleStream;
 	
+	/**
+	 * This creates an instance and initializes the {@link #round}, {@link #max}, and {@link #min}
+	 * functions.
+	 */
 	public ExpressionCalculator() {
 		
 		// initialize the custom calculator functions
@@ -55,28 +56,34 @@ public class ExpressionCalculator {
 			e.printStackTrace();
 		}
 	}
+	
 	/**
 	 * This calculates the value for a given expression, e.g. may be used in {@link AttributeValueConstraint}s or in {@link TargetSectionAttribute}s.
 	 * It represents only the general calculation rule.
-	 * @param expression for which the value have to be calculated or <em>null</em> 
-	 * @param vars contains all {@link FixedValue}s and {@link GlobalAttribute}s and temporarily {@link SourceSectionAttribute}s which are interpretable as double values.
+	 * 
+	 * @param expression The expression for which the value shall be calculated or <em>null</em> 
+	 * @param vars Contains all {@link FixedValue}s and {@link GlobalAttribute}s as well as values of 
+	 * {@link SourceSectionAttribute SourceSectionAttributes} which are interpretable as double values.
 	 * @return The calculated expression value or <em>null</em> if no value could be calculated.
 	 */
 	public String calculateExpression(String expression, Map<String, Double> vars){
 	
-		String expResult = "";
-		// make calculation
 		try{
-			expResult = String.valueOf(new ExpressionBuilder(expression)
+			// make calculation
+			//
+			return String.valueOf(new ExpressionBuilder(expression)
 				.withCustomFunction(round)
 				.withCustomFunction(max)
 				.withCustomFunction(min)
-				.withVariables(vars).build()
+				.withVariables(vars)
+				.build()
 				.calculate());
+			
 		} catch (final Exception e) {
+			
 			consoleStream.println("Message:\n" + e.getMessage() + "\n nothing calculated!");
 			return expression;
+			
 		}
-		return expResult;
 	}
 }

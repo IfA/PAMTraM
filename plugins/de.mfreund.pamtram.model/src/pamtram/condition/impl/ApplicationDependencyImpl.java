f259a71a6849eb6870623fc3b21e0eb2caf3f34e
mfreund <matth.freund@gmail.com>
2016-08-19 10:47:07 +0200

    Implement 'isLocalCondition' for 'ApplicationDependency'

--- plugins/de.mfreund.pamtram.model/src/pamtram/condition/impl/ApplicationDependencyImpl.java
+++ plugins/de.mfreund.pamtram.model/src/pamtram/condition/impl/ApplicationDependencyImpl.java
@@ -3,16 +3,18 @@
 package pamtram.condition.impl;
 
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.InternalEObject;
-
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 
 import pamtram.ConditionalElement;
-
 import pamtram.condition.ApplicationDependency;
 import pamtram.condition.ConditionPackage;
+import pamtram.mapping.InstantiableMappingHintGroup;
+import pamtram.mapping.Mapping;
+import pamtram.mapping.MappingHint;
+import pamtram.metamodel.SourceSection;
 
 /**
  * <!-- begin-user-doc -->
@@ -62,16 +64,18 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public ConditionalElement getConditionalElement() {
-		if (conditionalElement != null && conditionalElement.eIsProxy()) {
-			InternalEObject oldConditionalElement = (InternalEObject)conditionalElement;
-			conditionalElement = (ConditionalElement)eResolveProxy(oldConditionalElement);
-			if (conditionalElement != oldConditionalElement) {
-				if (eNotificationRequired())
-					eNotify(new ENotificationImpl(this, Notification.RESOLVE, ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT, oldConditionalElement, conditionalElement));
+		if (this.conditionalElement != null && this.conditionalElement.eIsProxy()) {
+			InternalEObject oldConditionalElement = (InternalEObject)this.conditionalElement;
+			this.conditionalElement = (ConditionalElement)this.eResolveProxy(oldConditionalElement);
+			if (this.conditionalElement != oldConditionalElement) {
+				if (this.eNotificationRequired()) {
+					this.eNotify(new ENotificationImpl(this, Notification.RESOLVE, ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT, oldConditionalElement, this.conditionalElement));
+				}
 			}
 		}
-		return conditionalElement;
+		return this.conditionalElement;
 	}
 
 	/**
@@ -80,7 +84,7 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	 * @generated
 	 */
 	public ConditionalElement basicGetConditionalElement() {
-		return conditionalElement;
+		return this.conditionalElement;
 	}
 
 	/**
@@ -88,11 +92,13 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public void setConditionalElement(ConditionalElement newConditionalElement) {
-		ConditionalElement oldConditionalElement = conditionalElement;
-		conditionalElement = newConditionalElement;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT, oldConditionalElement, conditionalElement));
+		ConditionalElement oldConditionalElement = this.conditionalElement;
+		this.conditionalElement = newConditionalElement;
+		if (this.eNotificationRequired()) {
+			this.eNotify(new ENotificationImpl(this, Notification.SET, ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT, oldConditionalElement, this.conditionalElement));
+		}
 	}
 
 	/**
@@ -104,8 +110,10 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
 			case ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT:
-				if (resolve) return getConditionalElement();
-				return basicGetConditionalElement();
+				if (resolve) {
+					return this.getConditionalElement();
+				}
+				return this.basicGetConditionalElement();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
@@ -119,7 +127,7 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
 			case ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT:
-				setConditionalElement((ConditionalElement)newValue);
+				this.setConditionalElement((ConditionalElement)newValue);
 				return;
 		}
 		super.eSet(featureID, newValue);
@@ -134,7 +142,7 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	public void eUnset(int featureID) {
 		switch (featureID) {
 			case ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT:
-				setConditionalElement((ConditionalElement)null);
+				this.setConditionalElement((ConditionalElement)null);
 				return;
 		}
 		super.eUnset(featureID);
@@ -149,9 +157,60 @@ public class ApplicationDependencyImpl extends ConditionImpl implements Applicat
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
 			case ConditionPackage.APPLICATION_DEPENDENCY__CONDITIONAL_ELEMENT:
-				return conditionalElement != null;
+				return this.conditionalElement != null;
 		}
 		return super.eIsSet(featureID);
+	}
+
+	@Override
+	public boolean isLocalCondition() {
+
+		Mapping mapping = null;
+		if (this.conditionalElement instanceof Mapping) {
+			mapping = (Mapping) this.conditionalElement;
+		} else if (this.conditionalElement instanceof InstantiableMappingHintGroup) {
+			mapping = (Mapping) this.conditionalElement.eContainer();
+		} else if (this.conditionalElement instanceof MappingHint) {
+			mapping = (Mapping) this.conditionalElement.eContainer().eContainer();
+		} else {
+			throw new RuntimeException(
+					"Unknown type of ConditionalElement '" + this.conditionalElement.eClass().getName() + "' found!");
+		}
+
+		SourceSection sourceSection = mapping.getSourceMMSection();
+
+		if (sourceSection == null) {
+			return false;
+		}
+
+		// The SourceSection that the condition references
+		//
+		SourceSection referencedSection = sourceSection.getContainingSection();
+
+		EObject container = this;
+
+		while (!(container instanceof Mapping)) {
+			if (container == null) {
+				return false;
+			}
+			container = container.eContainer();
+		}
+
+		// The SourceSection of the Mapping that contains the condition
+		//
+		SourceSection localSection = ((Mapping) container).getSourceMMSection();
+
+		if (referencedSection.equals(localSection)) {
+			return true;
+		}
+
+		// A condition is also 'local' if an InstancePointer with local or external SourceAttributes exist
+		//
+		return this.getAdditionalConditionSpecification().parallelStream()
+				.flatMap(instancePointer -> instancePointer.getSourceAttributes().parallelStream()
+						.filter(s -> s instanceof pamtram.metamodel.InstancePointerSourceElement
+								|| s instanceof pamtram.metamodel.InstancePointerExternalSourceElement))
+				.findAny().isPresent();
 	}
 
 } //ApplicationDependencyImpl

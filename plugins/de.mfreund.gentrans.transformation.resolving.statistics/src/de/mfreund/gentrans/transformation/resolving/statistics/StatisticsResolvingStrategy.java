cc2a1dedee12f1b92f849353f09cd4d3b4b80bde
mfreund <matth.freund@gmail.com>
2016-09-13 11:42:21 +0200

    First implementation of the StatisticsResolvingStrategy
    This represents a basic implementation that does only apply statistical
    analysis on the mapping model (not on the meta-model) level and does not
    handle ambiguities during the 'instantiating' phase.

--- plugins/de.mfreund.gentrans.transformation.resolving.statistics/src/de/mfreund/gentrans/transformation/resolving/statistics/StatisticsResolvingStrategy.java
+++ plugins/de.mfreund.gentrans.transformation.resolving.statistics/src/de/mfreund/gentrans/transformation/resolving/statistics/StatisticsResolvingStrategy.java
@@ -3,7 +3,31 @@
  */
 package de.mfreund.gentrans.transformation.resolving.statistics;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.jface.dialogs.DialogSettings;
+import org.eclipse.jface.dialogs.IDialogSettings;
+
+import de.mfreund.gentrans.transformation.descriptors.EObjectWrapper;
+import de.mfreund.gentrans.transformation.descriptors.MatchedSectionDescriptor;
+import de.mfreund.gentrans.transformation.descriptors.ModelConnectionPath;
 import de.mfreund.gentrans.transformation.resolving.AbstractAmbiguityResolvingStrategy;
+import de.mfreund.gentrans.transformation.resolving.IAmbiguityResolvedAdapter;
+import pamtram.PAMTraM;
+import pamtram.mapping.Mapping;
+import pamtram.mapping.MappingHintGroupType;
+import pamtram.metamodel.TargetSection;
+import pamtram.metamodel.TargetSectionClass;
+import pamtram.metamodel.TargetSectionNonContainmentReference;
 
 /**
  * This class implements a concrete {@link AbstractAmbiguityResolvingStrategy} that performs statistical evaluations of
@@ -14,6 +38,316 @@ import de.mfreund.gentrans.transformation.resolving.AbstractAmbiguityResolvingSt
  *
  * @author mfreund
  */
-public class StatisticsResolvingStrategy extends AbstractAmbiguityResolvingStrategy {
+public class StatisticsResolvingStrategy extends AbstractAmbiguityResolvingStrategy
+implements IAmbiguityResolvedAdapter {
 
+	/**
+	 * The instance of {@link IDialogSettings} that is used to store and retrieve statistics on the mapping model level.
+	 */
+	private IDialogSettings mappingSection;
+
+	/**
+	 * The instances of {@link IDialogSettings} that are used to store and retrieve statistics on the meta-model level.
+	 * <p />
+	 * The key of this map represents the {@link EPackage#getNsURI()} of a meta-model and the value represents the
+	 * associated {@link IDialogSettings}.
+	 */
+	private Map<String, IDialogSettings> metamodelSections;
+
+	@Override
+	public void init(PAMTraM pamtramModel, List<EObject> sourceModels, Logger logger)
+			throws AmbiguityResolvingException {
+
+		super.init(pamtramModel, sourceModels, logger);
+
+		// Acquire the various IDialogSettings that are used to persist/evaluate stored choices
+		//
+		IDialogSettings settings = StatisticsResolvingStrategyPlugin.getPlugin().getDialogSettings();
+		IDialogSettings section = DialogSettings.getOrCreateSection(settings, "STATISTICS");
+
+		IDialogSettings mapping = DialogSettings.getOrCreateSection(section, "MAPPING");
+
+		this.mappingSection = DialogSettings.getOrCreateSection(mapping,
+				pamtramModel.eResource().getURI().toFileString());
+
+		IDialogSettings metamodel = DialogSettings.getOrCreateSection(section, "METAMODEL");
+
+		Set<String> nsURIs = Stream
+				.concat(pamtramModel.getSourceSectionModel().stream(), pamtramModel.getTargetSectionModel().stream())
+				.map(sm -> sm.getMetaModelPackage().getNsURI()).collect(Collectors.toSet());
+
+		nsURIs.stream().forEach(nsURI -> DialogSettings.getOrCreateSection(metamodel, nsURI));
+
+	}
+
+	@Override
+	public List<MatchedSectionDescriptor> searchingSelectSection(List<MatchedSectionDescriptor> choices,
+			EObject element) throws AmbiguityResolvingException {
+
+		// We use the concatenated names of all sections in the list of choices as key
+		//
+		String key = String.join(";", choices.parallelStream().map(m -> m.getAssociatedSourceSectionClass().getName())
+				.sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// Sort the choices in descending order based on the number of previous count
+		//
+		return choices.parallelStream()
+				.sorted((o1, o2) -> StatisticsResolvingStrategy.this
+						.getInt(choicesSection, o2.getAssociatedSourceSectionClass().getName())
+						.compareTo(this.getInt(choicesSection, o1.getAssociatedSourceSectionClass().getName())))
+				.collect(Collectors.toList());
+
+	}
+
+	@Override
+	public void searchingSectionSelected(List<MatchedSectionDescriptor> choices,
+			MatchedSectionDescriptor resolved) {
+
+		// We use the concatenated names of all sections in the list of choices as key
+		//
+		String key = String.join(";", choices.parallelStream().map(m -> m.getAssociatedSourceSectionClass().getName())
+				.sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// The previous count for the given selected choice
+		//
+		int count = this.getInt(choicesSection, resolved.getAssociatedSourceSectionClass().getName());
+
+		choicesSection.put(resolved.getAssociatedSourceSectionClass().getName(), ++count);
+
+	}
+
+	@Override
+	public List<Mapping> searchingSelectMapping(List<Mapping> choices, EObject element)
+			throws AmbiguityResolvingException {
+
+		// We use the concatenated names of all mappings in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.parallelStream().map(m -> m.getName()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// Sort the choices in descending order based on the number of previous count
+		//
+		return choices.parallelStream()
+				.sorted((o1, o2) -> StatisticsResolvingStrategy.this
+						.getInt(choicesSection, o2.getName()).compareTo(this.getInt(choicesSection, o1.getName())))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void searchingMappingSelected(List<Mapping> choices, Mapping resolved) {
+
+		// We use the concatenated names of all mappings in the list of choices as key
+		//
+		String key = String.join(";", choices.parallelStream().map(m -> m.getName())
+				.sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// The previous count for the given selected choice
+		//
+		int count = this.getInt(choicesSection, resolved.getName());
+
+		choicesSection.put(resolved.getName(), ++count);
+
+	}
+
+	@Override
+	public void instantiatingAttributeValueSelected(List<String> choices, String resolved) {
+
+		// up to now, we do not apply statistical analysis in the instantiating phase so nothing is to be done here
+
+	}
+
+	@Override
+	public void instantiatingCardinalitySelected(List<Integer> choices, Integer resolved) {
+
+		// up to now, we do not apply statistical analysis in the instantiating phase so nothing is to be done here
+
+	}
+
+	@Override
+	public void joiningContainerInstanceSelected(List<EObjectWrapper> choices, EObjectWrapper resolved) {
+
+		// up to now, we do not apply statistical analysis on selected instances so nothing is to be done here
+
+	}
+
+	@Override
+	public Map<ModelConnectionPath, List<EObjectWrapper>> joiningSelectConnectionPathAndContainerInstance(
+			Map<ModelConnectionPath, List<EObjectWrapper>> choices, TargetSection section,
+			List<EObjectWrapper> sectionInstances, MappingHintGroupType hintGroup) throws AmbiguityResolvingException {
+
+		// We use the concatenated string representations of all paths in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.keySet().parallelStream().map(m -> m.toString()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// Sort the choices in descending order based on the number of previous count
+		// (we only sort the keys as, up to now, we do not perform statistical analysis on instances)
+		//
+		List<ModelConnectionPath> sortedKeys = choices.keySet()
+				.parallelStream().sorted((o1, o2) -> StatisticsResolvingStrategy.this
+						.getInt(choicesSection, o2.toString()).compareTo(this.getInt(choicesSection, o1.toString())))
+				.collect(Collectors.toList());
+
+		// We create a new LinkedHashMap as this guarantees ordering of keys
+		//
+		LinkedHashMap<ModelConnectionPath, List<EObjectWrapper>> sortedMap = new LinkedHashMap<>();
+		sortedKeys.stream().forEach(k -> sortedMap.put(k, choices.get(k)));
+
+		return sortedMap;
+	}
+
+	@Override
+	public List<ModelConnectionPath> joiningSelectConnectionPath(List<ModelConnectionPath> choices,
+			TargetSection section) throws AmbiguityResolvingException {
+
+		// We use the concatenated string representations of all paths in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.parallelStream().map(m -> m.toString()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// Sort the choices in descending order based on the number of previous count
+		//
+		return choices
+				.parallelStream().sorted((o1, o2) -> StatisticsResolvingStrategy.this
+						.getInt(choicesSection, o2.toString()).compareTo(this.getInt(choicesSection, o1.toString())))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void joiningConnectionPathSelected(List<ModelConnectionPath> choices, ModelConnectionPath resolved) {
+
+		// We use the concatenated string representations of all paths in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.parallelStream().map(m -> m.toString()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// The previous count for the given selected choice
+		//
+		int count = this.getInt(choicesSection, resolved.toString());
+
+		choicesSection.put(resolved.toString(), ++count);
+
+	}
+
+	@Override
+	public List<EClass> joiningSelectRootElement(List<EClass> choices) throws AmbiguityResolvingException {
+
+		// We use the concatenated names of all classes in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.parallelStream().map(m -> m.getName()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// Sort the choices in descending order based on the number of previous count
+		//
+		return choices
+				.parallelStream().sorted((o1, o2) -> StatisticsResolvingStrategy.this
+						.getInt(choicesSection, o2.getName()).compareTo(this.getInt(choicesSection, o1.getName())))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void joiningRootElementSelected(List<EClass> choices, EClass resolved) {
+
+		// We use the concatenated names of all classes in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.parallelStream().map(m -> m.getName()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// The previous count for the given selected choice
+		//
+		int count = this.getInt(choicesSection, resolved.getName());
+
+		choicesSection.put(resolved.getName(), ++count);
+
+	}
+
+	@Override
+	public void linkingTargetInstanceSelected(List<EObjectWrapper> choices, EObjectWrapper resolved) {
+
+		// up to now, we do not apply statistical analysis on selected instances so nothing is to be done here
+
+	}
+
+	@Override
+	public Map<TargetSectionClass, List<EObjectWrapper>> linkingSelectTargetSectionAndInstance(
+			Map<TargetSectionClass, List<EObjectWrapper>> choices, TargetSectionNonContainmentReference reference,
+			MappingHintGroupType hintGroup) throws AmbiguityResolvingException {
+
+		// We use the concatenated names of all classes in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.keySet().parallelStream().map(m -> m.getName()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// Sort the choices in descending order based on the number of previous count
+		// (we only sort the keys as, up to now, we do not perform statistical analysis on instances)
+		//
+		List<TargetSectionClass> sortedKeys = choices.keySet()
+				.parallelStream().sorted((o1, o2) -> StatisticsResolvingStrategy.this
+						.getInt(choicesSection, o2.getName()).compareTo(this.getInt(choicesSection, o1.getName())))
+				.collect(Collectors.toList());
+
+		// We create a new LinkedHashMap as this guarantees ordering of keys
+		//
+		LinkedHashMap<TargetSectionClass, List<EObjectWrapper>> sortedMap = new LinkedHashMap<>();
+		sortedKeys.stream().forEach(k -> sortedMap.put(k, choices.get(k)));
+
+		return sortedMap;
+	}
+
+	@Override
+	public void linkingTargetSectionSelected(List<TargetSectionClass> choices, TargetSectionClass resolved) {
+
+		// We use the concatenated names of all classes in the list of choices as key
+		//
+		String key = String.join(";",
+				choices.parallelStream().map(m -> m.getName()).sorted().collect(Collectors.toList()));
+
+		IDialogSettings choicesSection = DialogSettings.getOrCreateSection(this.mappingSection, key);
+
+		// The previous count for the given selected choice
+		//
+		int count = this.getInt(choicesSection, resolved.getName());
+
+		choicesSection.put(resolved.getName(), ++count);
+
+	}
+
+	/**
+	 * A helper method to extract an Integer value from an instance of {@link IDialogSettings}.
+	 *
+	 * @param settings
+	 *            The instance of {@link IDialogSettings} from that the Integer is to be extracted.
+	 * @param key
+	 *            The key that shall be used to extract the Integer value.
+	 * @return The extracted Integer value of '<em>0</em>' if no value could be extracted.
+	 */
+	private Integer getInt(IDialogSettings settings, String key) {
+
+		try {
+			return settings.getInt(key);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+
+	}
 }

11730db5bd47fc31a003aaddc7fe3bd359efa0ea
mfreund <matth.freund@gmail.com>
2016-09-28 13:40:54 +0200

    Improve the JoiningSelectRootElementMappingModelEnhancer

--- plugins/de.mfreund.gentrans.transformation.selectors/src/de/mfreund/gentrans/transformation/resolving/enhancing/JoiningSelectRootElementMappingModelEnhancer.java
+++ plugins/de.mfreund.gentrans.transformation.selectors/src/de/mfreund/gentrans/transformation/resolving/enhancing/JoiningSelectRootElementMappingModelEnhancer.java
@@ -6,6 +6,7 @@ package de.mfreund.gentrans.transformation.resolving.enhancing;
 import java.io.IOException;
 import java.util.List;
 
+import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -16,6 +17,9 @@ import de.mfreund.gentrans.transformation.resolving.UserDecisionResolvingStrateg
 import de.mfreund.gentrans.transformation.resolving.wizards.GenericSelectionDialogRunner;
 import pamtram.PAMTraM;
 import pamtram.PamtramPackage;
+import pamtram.mapping.Mapping;
+import pamtram.mapping.MappingFactory;
+import pamtram.mapping.MappingHintGroup;
 import pamtram.metamodel.MetamodelFactory;
 import pamtram.metamodel.TargetSection;
 import pamtram.presentation.PamtramEditor;
@@ -40,14 +44,29 @@ public class JoiningSelectRootElementMappingModelEnhancer extends MappingModelEn
 	@Override
 	public void widgetSelected(SelectionEvent e) {
 
+		// The TargetSection for the root element
+		//
 		TargetSection rootSection = MetamodelFactory.eINSTANCE.createTargetSection();
 		rootSection.setEClass(this.dialogRunner.getSingleSelection());
 		rootSection.setName(this.dialogRunner.getSingleSelection().getName() + " (Root)");
 
+		// The mapping for the root element TargetSection
+		//
+		Mapping mapping = MappingFactory.eINSTANCE.createMapping();
+		mapping.setName("RootMapping");
+		MappingHintGroup hintGroup = MappingFactory.eINSTANCE.createMappingHintGroup();
+		hintGroup.setName(rootSection.getName());
+		hintGroup.setTargetSection(rootSection);
+		mapping.getMappingHintGroups().add(hintGroup);
+
 		PamtramEditor editor = PamtramEditor.getEditor(this.pamtramModel, true);
 
 		if (editor == null) {
+
+			// Use the 'classic' way to add the new elements as we can not use any command stack
+			//
 			this.pamtramModel.getTargetSectionModel().get(0).getMetaModelSections().add(rootSection);
+			this.pamtramModel.getMappingModel().get(0).getMapping().add(mapping);
 
 			// finally, we save the model
 			try {
@@ -57,10 +76,17 @@ public class JoiningSelectRootElementMappingModelEnhancer extends MappingModelEn
 				e1.printStackTrace();
 			}
 		} else {
-			editor.getEditingDomain().getCommandStack()
-					.execute(new AddCommand(editor.getEditingDomain(),
-							editor.getPamtram().getTargetSectionModel().get(0),
+
+			// Use a command to add the new elements
+			//
+			CompoundCommand addCommand = new CompoundCommand();
+			addCommand.append(new AddCommand(editor.getEditingDomain(),
+					editor.getPamtram().getMappingModel().get(0),
+					PamtramPackage.Literals.MAPPING_MODEL__MAPPING, mapping));
+			addCommand.append(
+					new AddCommand(editor.getEditingDomain(), editor.getPamtram().getTargetSectionModel().get(0),
 							PamtramPackage.Literals.SECTION_MODEL__META_MODEL_SECTIONS, rootSection));
+			editor.getEditingDomain().getCommandStack().execute(addCommand);
 		}
 
 	}

79ca33b0370f55cdc822a9defb6f2cd4f2e53987
mfreund <matth.freund@gmail.com>
2016-11-03 09:53:22 +0100

    Allow the user to specify the location for exported SectionModels

--- plugins/de.mfreund.pamtram.model.editor/src/pamtram/handlers/ExportSectionModelHandler.java
+++ plugins/de.mfreund.pamtram.model.editor/src/pamtram/handlers/ExportSectionModelHandler.java
@@ -1,11 +1,10 @@
 package pamtram.handlers;
 
-import java.io.File;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -13,6 +12,7 @@ import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.SaveAsDialog;
 import org.eclipse.ui.part.FileEditorInput;
 
 import de.tud.et.ifa.agtele.ui.util.UIHelper;
@@ -31,6 +31,10 @@ import pamtram.presentation.PamtramEditor;
  */
 public class ExportSectionModelHandler extends AbstractHandler {
 
+	private static final String SOURCE_FILE_ENDING = ".pamtram.source";
+
+	private static final String TARGET_FILE_ENDING = ".pamtram.target";
+
 	@Override
 	public Object execute(ExecutionEvent event) {
 
@@ -38,23 +42,49 @@ public class ExportSectionModelHandler extends AbstractHandler {
 		//
 		SectionModel<?, ?, ?, ?> sectionModel = (SectionModel<?, ?, ?, ?>) UIHelper.getFirstSelection();
 
-		// The name of the new resource
-		// TODO the user should be able to change this via a dialog
-		//
-		String sectionModelResourceName = sectionModel.getName() + ".pamtram";
-
-		URI sectionModelResourceURI = sectionModel.eResource().getURI().trimSegments(1)
-				.appendSegment(sectionModelResourceName);
-
-		// Check if the resource that shall be created already exists
-		//
-		String sectionModelResourcePath = ResourcesPlugin.getWorkspace().getRoot()
-				.getFile(new Path(sectionModelResourceURI.toPlatformString(true))).getRawLocation().toOSString();
-
-		if (new File(sectionModelResourcePath).exists()) {
-			this.showError("Resource '" + sectionModelResourceName + "' already exits!");
+		if (!(UIHelper.getCurrentEditorInput() instanceof FileEditorInput)) {
 			return null;
 		}
+
+		IPath newPath = ((FileEditorInput) UIHelper.getCurrentEditorInput()).getPath().removeLastSegments(1)
+				.append(sectionModel.getName() + (sectionModel instanceof SourceSectionModel
+						? ExportSectionModelHandler.SOURCE_FILE_ENDING : ExportSectionModelHandler.TARGET_FILE_ENDING));
+		IFile newFile = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(newPath);
+
+		// Ask the user for the new location of the exported SectionModel
+		//
+		SaveAsDialog dialog = new SaveAsDialog(UIHelper.getShell());
+		dialog.setOriginalFile(newFile);
+		dialog.create();
+		dialog.setTitle("Export SectionModel");
+		if (sectionModel instanceof SourceSectionModel) {
+			dialog.setMessage("Specify the location for the exported SourceSectionModel (*"
+					+ ExportSectionModelHandler.SOURCE_FILE_ENDING + ")");
+		} else {
+			dialog.setMessage("Specify the location for the exported TargetSectionModel (*"
+					+ ExportSectionModelHandler.TARGET_FILE_ENDING + ")");
+		}
+		dialog.setBlockOnOpen(true);
+		dialog.open();
+		newPath = dialog.getResult();
+
+		if (newPath == null) {
+			return null;
+		}
+
+		// Validate the file name
+		//
+		if (sectionModel instanceof SourceSectionModel
+				&& !newPath.toString().endsWith(ExportSectionModelHandler.SOURCE_FILE_ENDING)) {
+			this.showError("Please specify a valid file name (*" + ExportSectionModelHandler.SOURCE_FILE_ENDING + ")");
+			return null;
+		} else if (sectionModel instanceof TargetSectionModel
+				&& !newPath.toString().endsWith(ExportSectionModelHandler.TARGET_FILE_ENDING)) {
+			this.showError("Please specify a valid file name (*" + ExportSectionModelHandler.TARGET_FILE_ENDING + ")");
+			return null;
+		}
+
+		URI sectionModelResourceURI = URI.createPlatformResourceURI(newPath.toString(), true);
 
 		// Determine the editing domain on which to execute the command that will perform the actual exporting
 		//

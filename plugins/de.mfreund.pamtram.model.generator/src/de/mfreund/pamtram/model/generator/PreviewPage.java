7f012ab5fa6e7c21346ef24a633c80522747f7d9
mfreund <matth.freund@gmail.com>
2016-11-28 10:15:48 +0100

    Fix: Do not display attributes in the tree viewer of the preview page

--- plugins/de.mfreund.pamtram.model.generator/src/de/mfreund/pamtram/model/generator/PreviewPage.java
+++ plugins/de.mfreund.pamtram.model.generator/src/de/mfreund/pamtram/model/generator/PreviewPage.java
@@ -1,7 +1,9 @@
 package de.mfreund.pamtram.model.generator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
@@ -32,34 +34,36 @@ import pamtram.metamodel.Section;
 import pamtram.metamodel.provider.MetamodelItemProviderAdapterFactory;
 
 /**
- * This {@link WizardPage} will display a preview of a generated source or target {@link Section} that
- * will get inserted into a pamtram model.
+ * This {@link WizardPage} will display a preview of a generated source or
+ * target {@link Section} that will get inserted into a pamtram model.
  *
  * @author mfreund
  */
 public class PreviewPage extends WizardPage {
 
 	/**
-	 * The {@link WizardData} that stores all relevant information gathered in the course of the wizard.
+	 * The {@link WizardData} that stores all relevant information gathered in
+	 * the course of the wizard.
 	 */
 	protected WizardData wizardData;
 
 	/**
-	 * The {@link ContainerCheckedTreeViewer} that will display the preview of the generated section
-	 * in a tree view.
+	 * The {@link ContainerCheckedTreeViewer} that will display the preview of
+	 * the generated section in a tree view.
 	 */
 	protected ContainerCheckedTreeViewer viewer;
 
 	/**
-	 * The {@link CheckboxTableViewer} that will display the preview of the attributes of the element
-	 * currently selected in the {@link #viewer}.
+	 * The {@link CheckboxTableViewer} that will display the preview of the
+	 * attributes of the element currently selected in the {@link #viewer}.
 	 */
 	protected CheckboxTableViewer propertiesViewer;
 
 	/**
-	 * This keeps track of those {@link MetaModelElement MetaModelElements} that have been unchecked by
-	 * the user (either in the {@link #viewer} or in the {@link #propertiesViewer} and that thus will not be
-	 * included in the section that will finally be added to the pamtram model.
+	 * This keeps track of those {@link MetaModelElement MetaModelElements} that
+	 * have been unchecked by the user (either in the {@link #viewer} or in the
+	 * {@link #propertiesViewer} and that thus will not be included in the
+	 * section that will finally be added to the pamtram model.
 	 */
 	protected List<MetaModelElement<?, ?, ?, ?>> elementsToExclude = new ArrayList<>();
 
@@ -71,7 +75,9 @@ public class PreviewPage extends WizardPage {
 	/**
 	 * This creates an instance.
 	 *
-	 * @param wizardData The {@link WizardData} that stores all relevant information gathered in the course of the wizard.
+	 * @param wizardData
+	 *            The {@link WizardData} that stores all relevant information
+	 *            gathered in the course of the wizard.
 	 */
 	public PreviewPage(WizardData wizardData) {
 		super("Preview");
@@ -105,15 +111,16 @@ public class PreviewPage extends WizardPage {
 				this.wizardData.setCreatedSections(new ArrayList<>());
 			}
 
-			if(this.wizardData.getCreatedSections() != null) {
+			if (this.wizardData.getCreatedSections() != null) {
 				this.viewer.setInput(this.wizardData.getCreatedSections().toArray());
 				// expand the tree so that the tree item map can be generated
 				this.viewer.expandAll();
 				for (Section<?, ?, ?, ?> clazz : this.wizardData.getCreatedSections()) {
 					this.viewer.setSubtreeChecked(clazz, true);
 				}
-				// collapse the tree (NOTE: 'collapseAll()' cannot be used as this disposes the tree items)
-				this.viewer.setExpandedTreePaths(new TreePath[]{});
+				// collapse the tree (NOTE: 'collapseAll()' cannot be used as
+				// this disposes the tree items)
+				this.viewer.setExpandedTreePaths(new TreePath[] {});
 				// only expand the root element and its direct children
 				this.viewer.expandToLevel(2);
 			}
@@ -126,80 +133,90 @@ public class PreviewPage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
-	
+
 		// container to host the file selector
 		Composite container = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.swtDefaults().numColumns(2).applyTo(container);
-	
+
 		// the viewer field is an already configured TreeViewer
 		this.viewer = new ContainerCheckedTreeViewer(container, SWT.H_SCROLL | SWT.V_SCROLL);
-	
-		this.viewer.setContentProvider(new EObjectTreeContentProvider());
+
+		this.viewer.setContentProvider(new EObjectTreeContentProvider() {
+
+			@Override
+			public Object[] getChildren(Object parentElement) {
+
+				return Arrays.asList(super.getChildren(parentElement)).parallelStream()
+						.filter(c -> !(c instanceof Attribute<?, ?, ?, ?>)).collect(Collectors.toList()).toArray();
+			}
+		});
 		this.viewer.setLabelProvider(new AdapterFactoryLabelProvider(this.adapterFactory));
-	
+
 		this.viewer.addCheckStateListener(event -> {
-			if(event.getElement() instanceof MetaModelElement) {
-				MetaModelElement<?, ?, ?, ?> element =
-						(MetaModelElement<?, ?, ?, ?>) event.getElement();
-				if(event.getChecked()) {
-					if(PreviewPage.this.elementsToExclude.contains(element)) {
+			if (event.getElement() instanceof MetaModelElement) {
+				MetaModelElement<?, ?, ?, ?> element = (MetaModelElement<?, ?, ?, ?>) event.getElement();
+				if (event.getChecked()) {
+					if (PreviewPage.this.elementsToExclude.contains(element)) {
 						PreviewPage.this.elementsToExclude.remove(element);
 					}
 				} else {
-					if(!PreviewPage.this.elementsToExclude.contains(element)) {
+					if (!PreviewPage.this.elementsToExclude.contains(element)) {
 						PreviewPage.this.elementsToExclude.add(element);
 					}
 				}
 			}
 		});
-	
+
 		// the tree that the viewer operates on
 		final Tree tree = (Tree) this.viewer.getControl();
 		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).hint(350, 400).applyTo(tree);
-	
-		// add a selection listener that - if a non-containment reference is selected
+
+		// add a selection listener that - if a non-containment reference is
+		// selected
 		// jumps to the referenced item in the tree
 		tree.addSelectionListener(new ResultPageTreeViewerSelectionListener());
-	
+
 		// create the properties viewer
 		this.propertiesViewer = CheckboxTableViewer.newCheckList(container, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
 		this.createTableViewerColumn("Property", 100);
 		this.createTableViewerColumn("Value", 150);
-	
+
 		// the table that the viewer operates on
 		final Table table = this.propertiesViewer.getTable();
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
-	
-		// use a simple array content provider to dipslay the array of attribute and their values
+
+		// use a simple array content provider to dipslay the array of attribute
+		// and their values
 		this.propertiesViewer
-		.setContentProvider((IStructuredContentProvider) inputElement -> inputElement instanceof List<?>
-		? ((List<?>) inputElement).toArray() : new Object[0]);
-	
+				.setContentProvider((IStructuredContentProvider) inputElement -> inputElement instanceof List<?>
+						? ((List<?>) inputElement).toArray() : new Object[0]);
+
 		this.propertiesViewer.setLabelProvider(new ResultPageTableViewerLabelProvider());
 		this.propertiesViewer.addCheckStateListener(event -> {
 			Attribute<?, ?, ?, ?> att = (Attribute<?, ?, ?, ?>) event.getElement();
-			if(event.getChecked()) {
-				if(PreviewPage.this.elementsToExclude.contains(att)) {
+			if (event.getChecked()) {
+				if (PreviewPage.this.elementsToExclude.contains(att)) {
 					PreviewPage.this.elementsToExclude.remove(att);
 				}
 			} else {
-				if(!PreviewPage.this.elementsToExclude.contains(att)) {
+				if (!PreviewPage.this.elementsToExclude.contains(att)) {
 					PreviewPage.this.elementsToExclude.add(att);
 				}
 			}
 		});
-	
+
 		// define layout for the table viewer
 		table.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true));
-	
+
 		// fill the wizard page
 		this.setControl(container);
 		this.setPageComplete(true);
 	}
 
 	/**
-	 * Create the {@link Section Section(s)} with the help of a {@link MetaModelSectionGenerator} and store them in the
+	 * Create the {@link Section Section(s)} with the help of a
+	 * {@link MetaModelSectionGenerator} and store them in the
 	 * {@link #wizardData}.
 	 *
 	 */
@@ -218,7 +235,8 @@ public class PreviewPage extends WizardPage {
 	}
 
 	/**
-	 * This creates a {@link TableViewerColumn} in the {@link #propertiesViewer}.
+	 * This creates a {@link TableViewerColumn} in the
+	 * {@link #propertiesViewer}.
 	 *
 	 * @param title
 	 *            The title of the column.
@@ -228,8 +246,7 @@ public class PreviewPage extends WizardPage {
 	 */
 	private TableViewerColumn createTableViewerColumn(String title, int width) {
 
-		final TableViewerColumn viewerColumn = new TableViewerColumn(this.propertiesViewer,
-				SWT.NONE);
+		final TableViewerColumn viewerColumn = new TableViewerColumn(this.propertiesViewer, SWT.NONE);
 		final TableColumn column = viewerColumn.getColumn();
 		column.setText(title);
 		column.setWidth(width);
@@ -250,15 +267,17 @@ public class PreviewPage extends WizardPage {
 	@Override
 	public boolean isPageComplete() {
 
-		// This can finish if at least one section has been created and selected by the user
+		// This can finish if at least one section has been created and selected
+		// by the user
 		//
 		return this.wizardData.getCreatedSections() != null && !this.wizardData.getCreatedSections().isEmpty()
 				&& this.viewer.getCheckedElements().length > 0;
 	}
 
 	/**
-	 * A {@link SelectionListener2 SelectionListener} that will update the {@link PreviewPage#propertiesViewer} based on
-	 * the selection in the {@link PreviewPage#viewer}.
+	 * A {@link SelectionListener2 SelectionListener} that will update the
+	 * {@link PreviewPage#propertiesViewer} based on the selection in the
+	 * {@link PreviewPage#viewer}.
 	 *
 	 * @author mfreund
 	 *
@@ -281,7 +300,7 @@ public class PreviewPage extends WizardPage {
 			// set the 'checked' states of the attributes
 			//
 			lines.stream().forEach(att -> {
-				if(!PreviewPage.this.elementsToExclude.contains(att)) {
+				if (!PreviewPage.this.elementsToExclude.contains(att)) {
 					PreviewPage.this.propertiesViewer.setChecked(att, true);
 				}
 			});

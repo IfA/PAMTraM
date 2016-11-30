73354880f3cb4840a68d8e4ed02779f312ac48e8
mfreund <matth.freund@gmail.com>
2016-11-30 10:58:12 +0100

    Convert the 'ClassMergeAction' to a 'StaticSelectionCommandAction'
    Instead of implementing the merge process in the action itself, use the
    new 'ClassMergeCommand' that will hopefully provide undo/redo support in
    the future.

--- plugins/de.mfreund.pamtram.model.editor/src/pamtram/actions/ClassMergeAction.java
+++ plugins/de.mfreund.pamtram.model.editor/src/pamtram/actions/ClassMergeAction.java
@@ -3,32 +3,27 @@
  */
 package pamtram.actions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.UnexecutableCommand;
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ContentViewer;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.ui.action.StaticSelectionCommandAction;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
 
-import de.tud.et.ifa.agtele.ui.util.UIHelper;
+import pamtram.commands.ClassMergeCommand;
 import pamtram.metamodel.Attribute;
 import pamtram.metamodel.Class;
 import pamtram.metamodel.Reference;
 import pamtram.metamodel.Section;
-import pamtram.util.ClassMerger;
 
 /**
- * An {@link Action} that can be used to merge multiple {@link Class Classes}.
+ * A {@link StaticSelectionCommandAction} that can be used to merge multiple {@link Class Classes} via the
+ * {@link ClassMergeCommand}.
  *
  * @param <S>
  * @param <C>
@@ -38,91 +33,75 @@ import pamtram.util.ClassMerger;
  * @author mfreund
  */
 public class ClassMergeAction<S extends Section<S, C, R, A>, C extends pamtram.metamodel.Class<S, C, R, A>, R extends Reference<S, C, R, A>, A extends Attribute<S, C, R, A>>
-		extends Action {
-
-	/**
-	 * The set of {@link Class elements} to be merged.
-	 */
-	protected Set<C> elementsToMerge = new HashSet<>();
-
-	/**
-	 * The list of {@link ContentViewer ContentViewers} to be refreshed after the merge.
-	 */
-	protected List<ContentViewer> viewersToUpdate;
+		extends StaticSelectionCommandAction {
 
 	/**
 	 * This creates an instance.
 	 *
-	 * @param text
-	 *            The String used as text for the action.
+	 * @param editorPart
+	 *            The {@link IEditorPart} in which this action is used.
 	 * @param selection
 	 *            The {@link IStructuredSelection selected elements} to be merged.
-	 * @param viewersToUpdate
-	 *            The list of {@link ContentViewer ContentViewers} to be refreshed after the merge.
 	 */
-	@SuppressWarnings("unchecked")
-	public ClassMergeAction(String text, IStructuredSelection selection, List<ContentViewer> viewersToUpdate) {
+	public ClassMergeAction(IEditorPart editorPart, IStructuredSelection selection) {
 
-		super(text);
+		super(editorPart);
+		this.setText("Merge selected elements");
 
-		this.viewersToUpdate = viewersToUpdate;
+		this.configureAction(selection);
 
-		// This action is only enabled if more than one elements are
-		// selected ...
-		//
-		boolean enabled = selection.size() > 1;
+	}
 
-		// And if all selected elements represent the same EClass
-		//
-		this.elementsToMerge = Stream.of(selection.toArray())
-				.filter(e -> e instanceof pamtram.metamodel.Class<?, ?, ?, ?>).map(e -> (C) e)
-				.collect(Collectors.toSet());
-		Set<EClass> eClasses = this.elementsToMerge.parallelStream().map(C::getEClass).collect(Collectors.toSet());
+	/**
+	 * This creates an instance.
+	 *
+	 * @param editingDomain
+	 *            The {@link EditingDomain} on which this the command represented by this action is to be executed.
+	 * @param selection
+	 *            The {@link IStructuredSelection selected elements} to be merged.
+	 */
+	public ClassMergeAction(EditingDomain editingDomain, IStructuredSelection selection) {
 
-		enabled = enabled && eClasses.size() == 1;
+		super(editingDomain);
+		this.setText("Merge selected elements");
 
-		this.setEnabled(enabled);
+		this.configureAction(selection);
+
 	}
 
 	@Override
-	public void run() {
+	protected Command createActionCommand(EditingDomain editingDomain, Collection<?> collection) {
 
-		// Merge the elements using the special merger class
+		// This action is only enabled if more than one element of the correct type 'C' are
+		// selected ...
 		//
-		ClassMerger<S, C, R, A> merger = new ClassMerger<>(this.elementsToMerge);
-
-		boolean canMerge = merger.canMerge();
-
-		if (!canMerge) {
-			MessageDialog.openError(UIHelper.getShell(), "Error", "The selected elements cannot be merged!");
-			return;
-		}
-
-		merger.merge();
-
-		// Finally, we delete the merged elements and update/refresh the viewers
-		//
-		Set<EObject> elementsToDelete = merger.getMergedElements().values().stream().flatMap(Set::stream)
-				.collect(Collectors.toSet());
-		elementsToDelete.addAll(merger.getDeletedElements());
-
-		elementsToDelete.stream().forEach(EcoreUtil::delete);
-
-		this.viewersToUpdate.stream().forEach(v -> {
-
-			if (v.getInput() instanceof Object[]) {
-				List<Object> input = new ArrayList<>(Arrays.asList((Object[]) v.getInput()));
-				input.removeAll(elementsToDelete);
-				v.setInput(input);
-			} else if (v.getInput() instanceof Collection<?>) {
-				List<Object> input = new ArrayList<>((Collection<?>) v.getInput());
-				input.removeAll(elementsToDelete);
-				v.setInput(input);
+		boolean enabled = collection.size() > 1 && collection.stream().allMatch(e -> {
+			try {
+				@SuppressWarnings({ "unchecked", "unused" })
+				C temp = (C) e;
+				return true;
+			} catch (ClassCastException e1) {
+				return false;
 			}
-
-			v.refresh();
 		});
 
+		if (!enabled) {
+			return UnexecutableCommand.INSTANCE;
+		}
+
+		// ... and if all selected elements represent the same EClass
+		//
+		@SuppressWarnings("unchecked")
+		Set<C> elementsToMerge = collection.stream().map(e -> (C) e).collect(Collectors.toSet());
+		Set<EClass> eClasses = elementsToMerge.parallelStream().map(C::getEClass).collect(Collectors.toSet());
+
+		enabled = eClasses.size() == 1;
+
+		if (!enabled) {
+			return UnexecutableCommand.INSTANCE;
+		}
+
+		return new ClassMergeCommand<>(editingDomain, elementsToMerge);
 	}
 
 }

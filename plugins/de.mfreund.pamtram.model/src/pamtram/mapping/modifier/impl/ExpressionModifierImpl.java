e4adcc00afff5234fa12bdec32ee98adeea10832
mfreund <matth.freund@gmail.com>
2017-01-12 16:07:12 +0100

    Implement 'modifyValue' for the new 'ExpressionModifier'

--- plugins/de.mfreund.pamtram.model/src/pamtram/mapping/modifier/impl/ExpressionModifierImpl.java
+++ plugins/de.mfreund.pamtram.model/src/pamtram/mapping/modifier/impl/ExpressionModifierImpl.java
@@ -2,33 +2,35 @@
  */
 package pamtram.mapping.modifier.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.ecore.EClass;
-
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 
+import de.mfreund.pamtram.util.ExpressionCalculator;
 import pamtram.mapping.modifier.ExpressionModifier;
 import pamtram.mapping.modifier.ModifierPackage;
 
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Expression Modifier</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> An implementation of the model object '<em><b>Expression Modifier</b></em>'. <!--
+ * end-user-doc -->
  * <p>
  * The following features are implemented:
  * </p>
  * <ul>
- *   <li>{@link pamtram.mapping.modifier.impl.ExpressionModifierImpl#getExpression <em>Expression</em>}</li>
+ * <li>{@link pamtram.mapping.modifier.impl.ExpressionModifierImpl#getExpression <em>Expression</em>}</li>
  * </ul>
  *
  * @generated
  */
 public class ExpressionModifierImpl extends NumericModifierImpl implements ExpressionModifier {
+
 	/**
-	 * The default value of the '{@link #getExpression() <em>Expression</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The default value of the '{@link #getExpression() <em>Expression</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 *
 	 * @see #getExpression()
 	 * @generated
 	 * @ordered
@@ -36,18 +38,18 @@ public class ExpressionModifierImpl extends NumericModifierImpl implements Expre
 	protected static final String EXPRESSION_EDEFAULT = "";
 
 	/**
-	 * The cached value of the '{@link #getExpression() <em>Expression</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The cached value of the '{@link #getExpression() <em>Expression</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 *
 	 * @see #getExpression()
 	 * @generated
 	 * @ordered
 	 */
-	protected String expression = EXPRESSION_EDEFAULT;
+	protected String expression = ExpressionModifierImpl.EXPRESSION_EDEFAULT;
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	protected ExpressionModifierImpl() {
@@ -55,108 +57,143 @@ public class ExpressionModifierImpl extends NumericModifierImpl implements Expre
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	@Override
 	protected EClass eStaticClass() {
+
 		return ModifierPackage.Literals.EXPRESSION_MODIFIER;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
+	@Override
 	public String getExpression() {
-		return expression;
+
+		return this.expression;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
+	@Override
 	public void setExpression(String newExpression) {
-		String oldExpression = expression;
-		expression = newExpression;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, ModifierPackage.EXPRESSION_MODIFIER__EXPRESSION, oldExpression, expression));
+
+		String oldExpression = this.expression;
+		this.expression = newExpression;
+		if (this.eNotificationRequired()) {
+			this.eNotify(new ENotificationImpl(this, Notification.SET, ModifierPackage.EXPRESSION_MODIFIER__EXPRESSION,
+					oldExpression, this.expression));
+		}
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+
 		switch (featureID) {
 			case ModifierPackage.EXPRESSION_MODIFIER__EXPRESSION:
-				return getExpression();
+				return this.getExpression();
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	@Override
 	public void eSet(int featureID, Object newValue) {
+
 		switch (featureID) {
 			case ModifierPackage.EXPRESSION_MODIFIER__EXPRESSION:
-				setExpression((String)newValue);
+				this.setExpression((String) newValue);
 				return;
 		}
 		super.eSet(featureID, newValue);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	@Override
 	public void eUnset(int featureID) {
+
 		switch (featureID) {
 			case ModifierPackage.EXPRESSION_MODIFIER__EXPRESSION:
-				setExpression(EXPRESSION_EDEFAULT);
+				this.setExpression(ExpressionModifierImpl.EXPRESSION_EDEFAULT);
 				return;
 		}
 		super.eUnset(featureID);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	@Override
 	public boolean eIsSet(int featureID) {
+
 		switch (featureID) {
 			case ModifierPackage.EXPRESSION_MODIFIER__EXPRESSION:
-				return EXPRESSION_EDEFAULT == null ? expression != null : !EXPRESSION_EDEFAULT.equals(expression);
+				return ExpressionModifierImpl.EXPRESSION_EDEFAULT == null ? this.expression != null
+						: !ExpressionModifierImpl.EXPRESSION_EDEFAULT.equals(this.expression);
 		}
 		return super.eIsSet(featureID);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 *
 	 * @generated
 	 */
 	@Override
 	public String toString() {
-		if (eIsProxy()) return super.toString();
+
+		if (this.eIsProxy()) {
+			return super.toString();
+		}
 
 		StringBuffer result = new StringBuffer(super.toString());
 		result.append(" (expression: ");
-		result.append(expression);
+		result.append(this.expression);
 		result.append(')');
 		return result.toString();
 	}
 
-} //ExpressionModifierImpl
+	@Override
+	public String modifyValue(String value) {
+
+		final Map<String, Double> vars = new HashMap<>();
+
+		// Expressions in an ExpressionModifier must contain only one variable 'x' - which is set to the given 'value'
+		//
+		try {
+			vars.put("x", Double.valueOf(value));
+		} catch (NumberFormatException e) {
+			System.out.println("Error parsing double of value '" + value + "'.");
+			return this.expression;
+		}
+
+		// Calculate the value
+		//
+		ExpressionCalculator expCalc = new ExpressionCalculator();
+		return expCalc.calculateExpression(this.expression, vars);
+	}
+
+} // ExpressionModifierImpl

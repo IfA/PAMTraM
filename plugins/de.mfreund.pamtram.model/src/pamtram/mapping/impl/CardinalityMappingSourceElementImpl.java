445192569f0e244fcdf2e2cfb59ed0dbbc4aa614
mfreund <matth.freund@gmail.com>
2017-02-20 17:16:25 +0100

    Implement 'getSourceAttribute' for the new elements

--- plugins/de.mfreund.pamtram.model/src/pamtram/mapping/impl/CardinalityMappingSourceElementImpl.java
+++ plugins/de.mfreund.pamtram.model/src/pamtram/mapping/impl/CardinalityMappingSourceElementImpl.java
@@ -5,32 +5,31 @@ package pamtram.mapping.impl;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
 
 import pamtram.mapping.CardinalityMappingSourceElement;
 import pamtram.mapping.CardinalityMappingSourceInterface;
 import pamtram.mapping.MappingHintSourceInterface;
 import pamtram.mapping.MappingPackage;
-
 import pamtram.structure.impl.LocalModifiedAttributeElementTypeImpl;
-
 import pamtram.structure.source.SourceSection;
 import pamtram.structure.source.SourceSectionAttribute;
 import pamtram.structure.source.SourceSectionClass;
 import pamtram.structure.source.SourceSectionReference;
 
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Cardinality Mapping Source Element</b></em>'.
+ * <!-- begin-user-doc --> An implementation of the model object '<em><b>Cardinality Mapping Source Element</b></em>'.
  * <!-- end-user-doc -->
  *
  * @generated
  */
-public class CardinalityMappingSourceElementImpl extends LocalModifiedAttributeElementTypeImpl<SourceSection, SourceSectionClass, SourceSectionReference, SourceSectionAttribute> implements CardinalityMappingSourceElement {
+public class CardinalityMappingSourceElementImpl extends
+		LocalModifiedAttributeElementTypeImpl<SourceSection, SourceSectionClass, SourceSectionReference, SourceSectionAttribute>
+		implements CardinalityMappingSourceElement {
+
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected CardinalityMappingSourceElementImpl() {
@@ -38,59 +37,65 @@ public class CardinalityMappingSourceElementImpl extends LocalModifiedAttributeE
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	protected EClass eStaticClass() {
+
 		return MappingPackage.Literals.CARDINALITY_MAPPING_SOURCE_ELEMENT;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
 	 */
+	@Override
 	public SourceSectionAttribute getSourceAttribute() {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
+
+		return this.getSource();
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public int eDerivedOperationID(int baseOperationID, Class<?> baseClass) {
+
 		if (baseClass == MappingHintSourceInterface.class) {
 			switch (baseOperationID) {
-				case MappingPackage.MAPPING_HINT_SOURCE_INTERFACE___GET_SOURCE_ATTRIBUTE: return MappingPackage.CARDINALITY_MAPPING_SOURCE_ELEMENT___GET_SOURCE_ATTRIBUTE;
-				default: return -1;
+				case MappingPackage.MAPPING_HINT_SOURCE_INTERFACE___GET_SOURCE_ATTRIBUTE:
+					return MappingPackage.CARDINALITY_MAPPING_SOURCE_ELEMENT___GET_SOURCE_ATTRIBUTE;
+				default:
+					return -1;
 			}
 		}
 		if (baseClass == CardinalityMappingSourceInterface.class) {
 			switch (baseOperationID) {
-				default: return -1;
+				default:
+					return -1;
 			}
 		}
 		return super.eDerivedOperationID(baseOperationID, baseClass);
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
+
 		switch (operationID) {
 			case MappingPackage.CARDINALITY_MAPPING_SOURCE_ELEMENT___GET_SOURCE_ATTRIBUTE:
-				return getSourceAttribute();
+				return this.getSourceAttribute();
 		}
 		return super.eInvoke(operationID, arguments);
 	}
 
-} //CardinalityMappingSourceElementImpl
+} // CardinalityMappingSourceElementImpl

ca38404727ab09588d45fb8c0dd7c637857251c8
mfreund <matthias.freund@tu-dresden.de>
2017-04-24 16:00:02 +0200

    Some manual changes to the ItemProviders

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/structure/source/provider/VirtualSourceSectionCrossReferenceItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/structure/source/provider/VirtualSourceSectionCrossReferenceItemProvider.java
@@ -2,15 +2,12 @@
  */
 package pamtram.structure.source.provider;
 
-
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
@@ -18,23 +15,22 @@ import org.eclipse.emf.edit.provider.StyledString;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
 import pamtram.provider.PamtramEditPlugin;
-
 import pamtram.structure.generic.provider.CrossReferenceItemProvider;
-
 import pamtram.structure.source.SourcePackage;
 import pamtram.structure.source.VirtualSourceSectionCrossReference;
 
 /**
- * This is the item provider adapter for a {@link pamtram.structure.source.VirtualSourceSectionCrossReference} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the item provider adapter for a
+ * {@link pamtram.structure.source.VirtualSourceSectionCrossReference} object.
+ * <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 public class VirtualSourceSectionCrossReferenceItemProvider extends CrossReferenceItemProvider {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public VirtualSourceSectionCrossReferenceItemProvider(AdapterFactory adapterFactory) {
@@ -42,131 +38,116 @@ public class VirtualSourceSectionCrossReferenceItemProvider extends CrossReferen
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
+		if (this.itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
-			addIgnoreUnmatchedElementsPropertyDescriptor(object);
-			addDerivationPropertyDescriptor(object);
+			this.addIgnoreUnmatchedElementsPropertyDescriptor(object);
+			this.addDerivationPropertyDescriptor(object);
 		}
-		return itemPropertyDescriptors;
+		return this.itemPropertyDescriptors;
 	}
 
 	/**
-	 * This adds a property descriptor for the Ignore Unmatched Elements feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Ignore Unmatched Elements
+	 * feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addIgnoreUnmatchedElementsPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_SourceSectionReference_ignoreUnmatchedElements_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_SourceSectionReference_ignoreUnmatchedElements_feature", "_UI_SourceSectionReference_type"),
-				 SourcePackage.Literals.SOURCE_SECTION_REFERENCE__IGNORE_UNMATCHED_ELEMENTS,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
-				 null,
-				 null));
+		this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(
+				((ComposeableAdapterFactory) this.adapterFactory).getRootAdapterFactory(), this.getResourceLocator(),
+				this.getString("_UI_SourceSectionReference_ignoreUnmatchedElements_feature"),
+				this.getString("_UI_PropertyDescriptor_description",
+						"_UI_SourceSectionReference_ignoreUnmatchedElements_feature",
+						"_UI_SourceSectionReference_type"),
+				SourcePackage.Literals.SOURCE_SECTION_REFERENCE__IGNORE_UNMATCHED_ELEMENTS, true, false, false,
+				ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE, null, null));
 	}
 
 	/**
-	 * This adds a property descriptor for the Derivation feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Derivation feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addDerivationPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_VirtualSourceSectionCrossReference_derivation_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_VirtualSourceSectionCrossReference_derivation_feature", "_UI_VirtualSourceSectionCrossReference_type"),
-				 SourcePackage.Literals.VIRTUAL_SOURCE_SECTION_CROSS_REFERENCE__DERIVATION,
-				 true,
-				 true,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
+		this.itemPropertyDescriptors.add(this.createItemPropertyDescriptor(
+				((ComposeableAdapterFactory) this.adapterFactory).getRootAdapterFactory(), this.getResourceLocator(),
+				this.getString("_UI_VirtualSourceSectionCrossReference_derivation_feature"),
+				this.getString("_UI_PropertyDescriptor_description",
+						"_UI_VirtualSourceSectionCrossReference_derivation_feature",
+						"_UI_VirtualSourceSectionCrossReference_type"),
+				SourcePackage.Literals.VIRTUAL_SOURCE_SECTION_CROSS_REFERENCE__DERIVATION, true, true, false,
+				ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
 	}
 
 	/**
-	 * This returns VirtualSourceSectionCrossReference.gif.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns VirtualSourceSectionCrossReference.gif. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/VirtualSourceSectionCrossReference"));
+		return this.overlayImage(object,
+				this.getResourceLocator().getImage("full/obj16/VirtualSourceSectionCrossReference"));
 	}
 
 	/**
-	 * This returns the label text for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the label text for the adapted class. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public String getText(Object object) {
-		return ((StyledString)getStyledText(object)).getString();
+		return ((StyledString) this.getStyledText(object)).getString();
 	}
-	
+
 	/**
-	 * This returns the label styled text for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * This returns the label styled text for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
 	 */
 	@Override
 	public Object getStyledText(Object object) {
-		String label = ((VirtualSourceSectionCrossReference)object).getName();
-    	StyledString styledLabel = new StyledString();
-		if (label == null || label.length() == 0) {
-			styledLabel.append(getString("_UI_VirtualSourceSectionCrossReference_type"), StyledString.Style.QUALIFIER_STYLER); 
-		} else {
-			styledLabel.append(getString("_UI_VirtualSourceSectionCrossReference_type"), StyledString.Style.QUALIFIER_STYLER).append(" " + label);
-		}
-		return styledLabel;
-	}	
+		return super.getStyledText(object);
+	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This handles model notifications by calling {@link #updateChildren} to
+	 * update any cached children and by creating a viewer notification, which
+	 * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void notifyChanged(Notification notification) {
-		updateChildren(notification);
+		this.updateChildren(notification);
 
 		switch (notification.getFeatureID(VirtualSourceSectionCrossReference.class)) {
-			case SourcePackage.VIRTUAL_SOURCE_SECTION_CROSS_REFERENCE__IGNORE_UNMATCHED_ELEMENTS:
-			case SourcePackage.VIRTUAL_SOURCE_SECTION_CROSS_REFERENCE__DERIVATION:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-				return;
+		case SourcePackage.VIRTUAL_SOURCE_SECTION_CROSS_REFERENCE__IGNORE_UNMATCHED_ELEMENTS:
+		case SourcePackage.VIRTUAL_SOURCE_SECTION_CROSS_REFERENCE__DERIVATION:
+			this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+			return;
 		}
 		super.notifyChanged(notification);
 	}
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing the children that can be created under this object. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -175,9 +156,9 @@ public class VirtualSourceSectionCrossReferenceItemProvider extends CrossReferen
 	}
 
 	/**
-	 * Return the resource locator for this item provider's resources.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Return the resource locator for this item provider's resources. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override

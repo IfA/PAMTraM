a02278f04a8c942a79a07f66eb6239bea2b781fc
mfreund <matthias.freund@tu-dresden.de>
2017-05-30 10:38:38 +0200

    Improve the label of the '(De-)Activate' menu contribution

--- plugins/de.mfreund.pamtram.model.editor/src/pamtram/contributions/DeactivateMenuContribution.java
+++ plugins/de.mfreund.pamtram.model.editor/src/pamtram/contributions/DeactivateMenuContribution.java
@@ -1,18 +1,27 @@
 package pamtram.contributions;
 
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.ui.ISelectionService;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.menus.CommandContributionItem;
 import org.eclipse.ui.menus.CommandContributionItemParameter;
 import org.eclipse.ui.menus.ExtensionContributionFactory;
 import org.eclipse.ui.menus.IContributionRoot;
 import org.eclipse.ui.services.IServiceLocator;
 
+import de.tud.et.ifa.agtele.ui.util.UIHelper;
 import pamtram.DeactivatableElement;
+import pamtram.MappingModel;
+import pamtram.SourceSectionModel;
+import pamtram.mapping.Mapping;
+import pamtram.mapping.MappingHintGroup;
+import pamtram.mapping.extended.MappingHint;
+import pamtram.structure.source.SourceSection;
 
 /**
  * This represents a menu contribution that allows to (de-)activate elements of
@@ -23,45 +32,53 @@ public class DeactivateMenuContribution extends ExtensionContributionFactory {
 	@Override
 	public void createContributionItems(IServiceLocator serviceLocator, IContributionRoot additions) {
 
-		ISelectionService s = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
-		if (!(s.getSelection() instanceof IStructuredSelection)) {
+		ISelection s = UIHelper.getCurrentSelection();
+		if (s == null || s.isEmpty() || !(s instanceof IStructuredSelection)) {
 			return;
 		}
 
-		IStructuredSelection selection = (IStructuredSelection) s.getSelection();
+		IStructuredSelection selection = (IStructuredSelection) s;
 
 		/*
 		 * Check if all selected elements are of type 'DeactivatableElement' and
 		 * have the same activation status.
 		 */
-		Iterator<?> it = selection.iterator();
-		boolean status = false;
-		while (it.hasNext()) {
-			Object el = it.next();
-			if (!(el instanceof DeactivatableElement)) {
-				return;
-			}
-			boolean deactivated = ((DeactivatableElement) el).isDeactivated();
-			if (deactivated != status) {
-				if (selection.getFirstElement().equals(el)) {
-					status = deactivated;
-				} else {
-					return;
-				}
-			}
-
+		List<Object> selectedElements = Arrays.asList(selection.toArray());
+		if (!selectedElements.parallelStream().allMatch(el -> el instanceof DeactivatableElement) || selectedElements
+				.parallelStream().map(el -> ((DeactivatableElement) el)).collect(Collectors.toSet()).size() > 1) {
+			return;
 		}
+
+		boolean status = ((DeactivatableElement) selectedElements.get(0)).isDeactivated();
+
+		boolean allElementsOfSameType = selectedElements.parallelStream()
+				.map(el -> ((DeactivatableElement) el).eClass()).collect(Collectors.toSet()).size() == 1;
 
 		/*
 		 * Create the contribution
 		 */
 		CommandContributionItemParameter p = new CommandContributionItemParameter(serviceLocator, "",
 				"de.mfreund.pamtram.commands.DeactivationCommand", SWT.PUSH);
-		p.label = status ? "Activate" : "Deactivate";
-		// p.icon =
-		// Implementation.imageDescriptorFromPlugin("de.mfreund.pamtram.model.editor",
-		// "icons/)
-		// p.icon = Activator.getImageDescriptor("icons/alt_window_16.gif");
+		String label = "PAMTraM: " + (status ? "Activate" : "Deactivate");
+
+		if (allElementsOfSameType) {
+			EObject firstElement = (DeactivatableElement) selectedElements.get(0);
+			if (firstElement instanceof SourceSection) {
+				label += " SourceSection";
+			} else if (firstElement instanceof SourceSectionModel) {
+				label += " SourceSectionModel";
+			} else if (firstElement instanceof MappingModel) {
+				label += " MappingModel";
+			} else if (firstElement instanceof Mapping) {
+				label += " Mapping";
+			} else if (firstElement instanceof MappingHintGroup) {
+				label += " MappingHintGroup";
+			} else if (firstElement instanceof MappingHint) {
+				label += " MappingHint";
+			}
+		}
+
+		p.label = label;
 
 		CommandContributionItem item = new CommandContributionItem(p);
 		item.setVisible(true);

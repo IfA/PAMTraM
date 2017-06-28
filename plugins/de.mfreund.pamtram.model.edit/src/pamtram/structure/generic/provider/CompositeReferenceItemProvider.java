eaf3e84ecac900de85796fe572329147bf4c2d82
mfreund <matthias.freund@tu-dresden.de>
2017-06-28 17:00:50 +0200

    Prevent an NPE in the CompositeReferenceItemProvider

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/structure/generic/provider/CompositeReferenceItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/structure/generic/provider/CompositeReferenceItemProvider.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.StyledString;
@@ -47,36 +48,41 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 	 */
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
+		if (this.itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
 		}
-		return itemPropertyDescriptors;
+		return this.itemPropertyDescriptors;
 	}
 
 	/**
-	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * This specifies how to implement {@link #getChildren} and is used to
+	 * deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand},
+	 * {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in
+	 * {@link #createCommand}. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
-		if (childrenFeatures == null) {
+		if (this.childrenFeatures == null) {
 			super.getChildrenFeatures(object);
-			childrenFeatures.add(GenericPackage.Literals.COMPOSITE_REFERENCE__VALUE);
+			this.childrenFeatures.add(GenericPackage.Literals.COMPOSITE_REFERENCE__VALUE);
 		}
-		return childrenFeatures;
+		return this.childrenFeatures;
 	}
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	protected EStructuralFeature getChildFeature(Object object, Object child) {
-		// Check the type of the specified child object and return the proper feature to use for
+		// Check the type of the specified child object and return the proper
+		// feature to use for
 		// adding (see {@link AddCommand}) it as a child.
 
 		return super.getChildFeature(object, child);
@@ -92,14 +98,14 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 	}
 
 	/**
-	 * This returns the label text for the adapted class.
-	 * <!-- begin-user-doc
+	 * This returns the label text for the adapted class. <!-- begin-user-doc
 	 * --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public String getText(Object object) {
-		return ((StyledString)getStyledText(object)).getString();
+		return ((StyledString) this.getStyledText(object)).getString();
 	}
 
 	/**
@@ -115,20 +121,21 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc --> <!--
+	 * This handles model notifications by calling {@link #updateChildren} to
+	 * update any cached children and by creating a viewer notification, which
+	 * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
 	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public void notifyChanged(Notification notification) {
-		updateChildren(notification);
+		this.updateChildren(notification);
 
 		switch (notification.getFeatureID(CompositeReference.class)) {
-			case GenericPackage.COMPOSITE_REFERENCE__VALUE:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-				return;
+		case GenericPackage.COMPOSITE_REFERENCE__VALUE:
+			this.fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+			return;
 		}
 		super.notifyChanged(notification);
 	}
@@ -146,8 +153,10 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
 
 		Class<?, ?, ?, ?> clazz = SourceFactory.eINSTANCE.createSourceSectionClass();
-		if (object instanceof ActualReference<?, ?, ?, ?>
-				&& ((ActualReference<?, ?, ?, ?>) object).getEReference().getUpperBound() < 0) {
+
+		EReference ref = object instanceof ActualReference<?, ?, ?, ?>
+				? ((ActualReference<?, ?, ?, ?>) object).getEReference() : null;
+		if (ref != null && ref.getUpperBound() < 0) {
 			clazz.setCardinality(CardinalityType.ONE_INFINITY);
 		}
 
@@ -160,8 +169,7 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 		// StructureFactory.eINSTANCE.createSourceSection()));
 
 		clazz = TargetFactory.eINSTANCE.createTargetSectionClass();
-		if (object instanceof ActualReference<?, ?, ?, ?>
-				&& ((ActualReference<?, ?, ?, ?>) object).getEReference().getUpperBound() > 0) {
+		if (ref != null && ref.getUpperBound() > 0) {
 			clazz.setCardinality(CardinalityType.ONE_INFINITY);
 		}
 

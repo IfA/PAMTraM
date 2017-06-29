38caabd3639c95ecce0a7cfdfaa9e4e093ff72b4
mfreund <matthias.freund@tu-dresden.de>
2017-06-29 09:32:28 +0200

    Edit: Minor fix for setting cardinalities of new Classes

--- plugins/de.mfreund.pamtram.model.edit/src/pamtram/structure/generic/provider/CompositeReferenceItemProvider.java
+++ plugins/de.mfreund.pamtram.model.edit/src/pamtram/structure/generic/provider/CompositeReferenceItemProvider.java
@@ -62,7 +62,7 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 	 * {@link org.eclipse.emf.edit.command.RemoveCommand} or
 	 * {@link org.eclipse.emf.edit.command.MoveCommand} in
 	 * {@link #createCommand}. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
+	 *
 	 * @generated
 	 */
 	@Override
@@ -76,7 +76,7 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
+	 *
 	 * @generated
 	 */
 	@Override
@@ -100,7 +100,7 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 	/**
 	 * This returns the label text for the adapted class. <!-- begin-user-doc
 	 * --> <!-- end-user-doc -->
-	 * 
+	 *
 	 * @generated
 	 */
 	@Override
@@ -125,7 +125,7 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 	 * update any cached children and by creating a viewer notification, which
 	 * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
 	 * end-user-doc -->
-	 * 
+	 *
 	 * @generated
 	 */
 	@Override
@@ -169,7 +169,7 @@ public class CompositeReferenceItemProvider extends ReferenceItemProvider {
 		// StructureFactory.eINSTANCE.createSourceSection()));
 
 		clazz = TargetFactory.eINSTANCE.createTargetSectionClass();
-		if (ref != null && ref.getUpperBound() > 0) {
+		if (ref != null && ref.getUpperBound() < 0) {
 			clazz.setCardinality(CardinalityType.ONE_INFINITY);
 		}
 

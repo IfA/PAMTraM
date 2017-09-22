4f61eb3d4f3df57c8606215781329ac6ea78080b
mfreund <matthias.freund@tu-dresden.de>
2017-09-22 10:17:14 +0200

    GenTrans: Allow adding more values to an existing 'GlobalValueMap'

--- plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/maps/GlobalValueMap.java
+++ plugins/de.mfreund.gentrans.transformation/src/de/mfreund/gentrans/transformation/maps/GlobalValueMap.java
@@ -11,9 +11,8 @@ import pamtram.NamedElement;
 import pamtram.mapping.GlobalAttribute;
 
 /**
- * This is used to store values of {@link FixedValue FixedValues} and
- * {@link GlobalAttribute GlobalAttributes} that can be used throughout the
- * transformation.
+ * This is used to store values of {@link FixedValue FixedValues} and {@link GlobalAttribute GlobalAttributes} that can
+ * be used throughout the transformation.
  *
  * @author mfreund
  *
@@ -26,20 +25,17 @@ public class GlobalValueMap {
 	private Map<FixedValue, String> fixedValues;
 
 	/**
-	 * This keeps track of the values of {@link GlobalAttribute
-	 * GlobalAttributes}.
+	 * This keeps track of the values of {@link GlobalAttribute GlobalAttributes}.
 	 */
 	private Map<GlobalAttribute, String> globalAttributes;
 
 	/**
-	 * This keeps track of both {@link FixedValue FixedValues} and
-	 * {@link GlobalAttribute GlobalAttributes}.
+	 * This keeps track of both {@link FixedValue FixedValues} and {@link GlobalAttribute GlobalAttributes}.
 	 */
 	private Map<NamedElement, String> globalValues;
 
 	/**
-	 * This keeps track of a those of the {@link #globalValues} that can be
-	 * parsed as double.
+	 * This keeps track of a those of the {@link #globalValues} that can be parsed as double.
 	 */
 	private Map<NamedElement, Double> globalValuesAsDouble;
 
@@ -62,14 +58,40 @@ public class GlobalValueMap {
 	}
 
 	/**
+	 * Add a list of {@link FixedValue FixedValues} to this map.
+	 *
+	 * @param fixedValues
+	 *            the {@link FixedValue FixedValues} to add
+	 */
+	public void addFixedValues(Map<FixedValue, String> fixedValues) {
+
+		this.fixedValues.putAll(fixedValues);
+		this.globalValues.putAll(fixedValues);
+		this.globalValuesAsDouble = null;
+	}
+
+	/**
+	 * Add a list of values for {@link GlobalAttribute GlobalAttributes} to this map.
+	 *
+	 * @param globalAttributeValues
+	 *            the values of {@link GlobalAttribute GlobalAttributes} to add
+	 */
+	public void addGlobalAttributeValues(Map<GlobalAttribute, String> globalAttributeValues) {
+
+		this.globalAttributes.putAll(globalAttributeValues);
+		this.globalValues.putAll(globalAttributeValues);
+		this.globalValuesAsDouble = null;
+	}
+
+	/**
 	 * Returns the value of the given {@link FixedValue}.
 	 *
 	 * @param fixedValue
 	 *            The {@link FixedValue} for that the value shall be returned.
-	 * @return The value or '<em><b>null</b></em> if no value for the given
-	 *         <em>fixedValue</em> exists.
+	 * @return The value or '<em><b>null</b></em> if no value for the given <em>fixedValue</em> exists.
 	 */
 	public String get(FixedValue fixedValue) {
+
 		return this.fixedValues.get(fixedValue);
 	}
 
@@ -77,56 +99,52 @@ public class GlobalValueMap {
 	 * Returns the value of the given {@link GlobalAttribute}.
 	 *
 	 * @param globalAttribute
-	 *            The {@link GlobalAttribute} for that the value shall be
-	 *            returned.
-	 * @return The value or '<em><b>null</b></em> if no value for the given
-	 *         <em>globalAttribute</em> exists.
+	 *            The {@link GlobalAttribute} for that the value shall be returned.
+	 * @return The value or '<em><b>null</b></em> if no value for the given <em>globalAttribute</em> exists.
 	 */
 	public String get(GlobalAttribute globalAttribute) {
-		return this.fixedValues.get(globalAttribute);
+
+		return this.globalAttributes.get(globalAttribute);
 	}
 
 	/**
 	 * This is the getter for the {@link #fixedValues}.
 	 *
-	 * @return The values of the {@link FixedValue FixedValues} modeled in the
-	 *         PAMTraM model.
+	 * @return The values of the {@link FixedValue FixedValues} modeled in the PAMTraM model.
 	 */
 	public Map<FixedValue, String> getFixedValues() {
+
 		return this.fixedValues;
 	}
 
 	/**
 	 * This is the getter for the {@link #globalAttributes}.
 	 *
-	 * @return The extracted values of the {@link GlobalAttribute
-	 *         GlobalAttributes} modeled in the PAMTraM model.
+	 * @return The extracted values of the {@link GlobalAttribute GlobalAttributes} modeled in the PAMTraM model.
 	 */
 	public Map<GlobalAttribute, String> getGlobalAttributes() {
+
 		return this.globalAttributes;
 	}
 
 	/**
 	 * This is the getter for the {@link #globalValues}.
 	 *
-	 * @return The values of both {@link FixedValue FixedValues} and
-	 *         {@link GlobalAttribute GlobalAttributes}.
+	 * @return The values of both {@link FixedValue FixedValues} and {@link GlobalAttribute GlobalAttributes}.
 	 */
 	public Map<NamedElement, String> getGlobalValues() {
+
 		return this.globalValues;
 	}
 
 	/**
-	 * This returns the values of the {@link #globalValues} as {@link Double}
-	 * values that can be used in calculations.
+	 * This returns the values of the {@link #globalValues} as {@link Double} values that can be used in calculations.
 	 * <p />
-	 * Therefore, it tries to parse a <em>double</em> value for each of the
-	 * {@link #globalValues}. Values that cannot be parsed as <em>double</em>
-	 * are ignored.
+	 * Therefore, it tries to parse a <em>double</em> value for each of the {@link #globalValues}. Values that cannot be
+	 * parsed as <em>double</em> are ignored.
 	 *
-	 * @return The values of both {@link FixedValue FixedValues} and
-	 *         {@link GlobalAttribute GlobalAttributes} represented as
-	 *         <em>double</em>.
+	 * @return The values of both {@link FixedValue FixedValues} and {@link GlobalAttribute GlobalAttributes}
+	 *         represented as <em>double</em>.
 	 */
 	public Map<NamedElement, Double> getAsDouble() {
 
@@ -141,8 +159,7 @@ public class GlobalValueMap {
 				try {
 
 					/*
-					 * We make use of the ExpressionBuilder as
-					 * 'String.valueOf(double)' doesn't support scientific
+					 * We make use of the ExpressionBuilder as 'String.valueOf(double)' doesn't support scientific
 					 * notation, like: 0.42e2 == 4200e-2 == 42
 					 */
 					final Calculable calc = new ExpressionBuilder(globalValue.getValue()).build();
@@ -158,18 +175,16 @@ public class GlobalValueMap {
 	}
 
 	/**
-	 * This returns the values of the {@link #globalValues} as {@link Double}
-	 * values that can be used in calculations. Thereby, the
-	 * {@link NamedElement#getName() names} of the respective elements are used
-	 * as key in the returned map.
+	 * This returns the values of the {@link #globalValues} as {@link Double} values that can be used in calculations.
+	 * Thereby, the {@link NamedElement#getName() names} of the respective elements are used as key in the returned map.
 	 *
 	 * @see #getAsDouble()
 	 *
-	 * @return The values of both {@link FixedValue FixedValues} and
-	 *         {@link GlobalAttribute GlobalAttributes} represented as
-	 *         <em>double</em>.
+	 * @return The values of both {@link FixedValue FixedValues} and {@link GlobalAttribute GlobalAttributes}
+	 *         represented as <em>double</em>.
 	 */
 	public Map<String, Double> getAsDoubleByString() {
+
 		return this.getAsDouble().entrySet().stream()
 				.collect(Collectors.toMap(e -> e.getKey().getName(), e -> e.getValue()));
 	}

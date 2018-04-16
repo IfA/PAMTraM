1691b58e4e9ab2e50c0d5c8b479d9f9b4420f5b3
mfreund <matthias.freund@tu-dresden.de>
2018-04-16 11:52:43 +0200

    Various fixes/additions for EPL2 copyright notes

--- casestudies/dfd-uml/data-flow-diagram.model/src/dfd/ControlFlow.java
+++ casestudies/dfd-uml/data-flow-diagram.model/src/dfd/ControlFlow.java
@@ -1,21 +1,17 @@
 /*******************************************************************************
  * Copyright (C) 2017-2018 Matthias Freund and others, Institute of Automation, TU Dresden
- * 
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
+ * Contributors: Institute of Automation, TU Dresden - Initial API and implementation
+ *
  * SPDX-License-Identifier: EPL-2.0
  ******************************************************************************/
-/**
- */
 package dfd;
 
-
 /**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Control Flow</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Control Flow</b></em>'. <!-- end-user-doc -->
  *
  *
  * @see dfd.DfdPackage#getControlFlow()
--- plugins/de.mfreund.gentrans.ui/src/de/mfreund/gentrans/ui/launching/GentransLaunchContext.java
+++ plugins/de.mfreund.gentrans.ui/src/de/mfreund/gentrans/ui/launching/GentransLaunchContext.java
@@ -5,6 +5,9 @@
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
+ * Contributors:
+ *   Institute of Automation, TU Dresden - Initial API and implementation
+ * 
  * SPDX-License-Identifier: EPL-2.0
  ******************************************************************************/
 package de.mfreund.gentrans.ui.launching;
